0114518edbb64d23c91ad3077e1177c28000b050
Matthew Parker <pingu@pinguapps.com>
2024-06-18 03:06:53 +0100

    Updated IsExternalInit to what better represents it

--- PinguApps.Appwrite.Common/IsExternalInit.cs
+++ PinguApps.Appwrite.Common/IsExternalInit.cs
@@ -2,12 +2,9 @@
 
 namespace System.Runtime.CompilerServices;
 
-/// <summary>
-/// Reserved to be used by the compiler for tracking metadata.
-/// This class should not be used by developers in source code.
-/// This dummy class is required to compile records when targeting .NET Standard
-/// </summary>
+#if !NET5_0_OR_GREATER
+
 [EditorBrowsable(EditorBrowsableState.Never)]
-public static class IsExternalInit
-{
-}
+internal static class IsExternalInit { }
+
+#endif // !NET5_0_OR_GREATER
