6677416f60b7dc5f24156a9e580841064f22992b
Matthew Parker <pingu@pinguapps.com>
2024-07-06 17:44:07 +0100

    Added Service Collection Extensions tests to server proj

---
+++ tests/PinguApps.Appwrite.Server.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PinguApps.Appwrite.Server.Handlers;
+using PinguApps.Appwrite.Server.Internals;
+using PinguApps.Appwrite.Server.Servers;
+using PinguApps.Appwrite.Shared.Tests;
+
+namespace PinguApps.Appwrite.Server.Tests;
+public class ServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddAppwriteServer_RegistersExpectedServices()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddAppwriteServer(Constants.ProjectId, Constants.ApiKey, Constants.Endpoint);
+
+        // Assert
+        var provider = services.BuildServiceProvider();
+
+        // Assert HeaderHandler is registered
+        var headerHandler = provider.GetService<HeaderHandler>();
+        Assert.NotNull(headerHandler);
+
+        // Assert IAccountApi is registered and configured
+        var accountApi = provider.GetService<IAccountApi>();
+        Assert.NotNull(accountApi);
+
+        // Assert services are registered
+        Assert.NotNull(provider.GetService<IAccountServer>());
+        Assert.NotNull(provider.GetService<IAppwriteServer>());
+    }
+
+    // Additional tests can be added to verify different configurations or overloads of AddAppwriteServer
+}
