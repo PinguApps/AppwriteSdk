8e64165cc004e88401850853eb93d2e699661024
Matthew Parker <pingu@pinguapps.com>
2024-07-07 00:53:45 +0100

    added coverlet and test script to other test projects

---
+++ tests/PinguApps.Appwrite.Server.Tests/test.ps1
@@ -0,0 +1,15 @@
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+
+# Check if the dotnet-reportgenerator-globaltool is installed
+$toolInstalled = dotnet tool list -g | Select-String -Pattern "dotnet-reportgenerator-globaltool"
+
+if (-not $toolInstalled) {
+    # Install the dotnet-reportgenerator-globaltool globally
+    dotnet tool install -g dotnet-reportgenerator-globaltool
+}
+
+# Generate the report
+reportgenerator -reports:coverage.opencover.xml -targetdir:coverage-report
+
+# Open the generated report in the default browser
+Start-Process "coverage-report/index.html"
