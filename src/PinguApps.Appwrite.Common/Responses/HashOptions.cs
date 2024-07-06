6863cc21a09eacec67f6bcf08de7b288da2a31e9
Matthew Parker <pingu@pinguapps.com>
2024-07-07 00:09:59 +0100

    Create HashOptions.cs

---
+++ src/PinguApps.Appwrite.Common/Responses/HashOptions.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PinguApps.Appwrite.Shared.Responses;
+public record HashOptions(
+    [property: JsonPropertyName("type")] string Type,
+    [property: JsonPropertyName("memoryCost")] long MemoryCost,
+    [property: JsonPropertyName("timeCost")] int TimeCost,
+    [property: JsonPropertyName("threads")] int Threads
+);
