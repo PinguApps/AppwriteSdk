45d76bff9819aad0dbbda33583098a7a5c5a41c6
Matthew Parker <pingu@pinguapps.com>
2024-07-07 00:09:29 +0100

    Create InternalErrorTests.cs

---
+++ tests/PinguApps.Appwrite.Shared.Tests/InternalErrorTests.cs
@@ -0,0 +1,16 @@
+﻿namespace PinguApps.Appwrite.Shared.Tests;
+public class InternalErrorTests
+{
+    [Fact]
+    public void Constructor_AssignsMessage()
+    {
+        // Arrange
+        var expectedMessage = "An error occurred";
+
+        // Act
+        var internalError = new InternalError(expectedMessage);
+
+        // Assert
+        Assert.Equal(expectedMessage, internalError.Message);
+    }
+}
