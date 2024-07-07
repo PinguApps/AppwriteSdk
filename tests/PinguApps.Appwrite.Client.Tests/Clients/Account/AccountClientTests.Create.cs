22ec938595d99e34773c72374a26ce3adade443f
Matthew Parker <pingu@pinguapps.com>
2024-07-07 01:48:18 +0100

    Split AccountClient into partials

---
+++ tests/PinguApps.Appwrite.Client.Tests/Clients/Account/AccountClientTests.Create.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Requests;
+using PinguApps.Appwrite.Shared.Tests;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Client.Tests.Clients.Account;
+public partial class AccountClientTests
+{
+    [Fact]
+    public async Task Create_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        var request = new CreateAccountRequest()
+        {
+            Email = "email@example.com",
+            Password = "password",
+            Name = "name"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account")
+            .ExpectedHeaders()
+            .WithJsonContent(request)
+            .Respond(Constants.AppJson, Constants.UserResponse);
+
+        // Act
+        var result = await _appwriteClient.Account.Create(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task Create_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new CreateAccountRequest()
+        {
+            Email = "email@example.com",
+            Password = "password",
+            Name = "name"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account")
+            .ExpectedHeaders()
+            .WithJsonContent(request)
+            .Respond(HttpStatusCode.BadRequest, Constants.AppJson, Constants.AppwriteError);
+
+        // Act
+        var result = await _appwriteClient.Account.Create(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task Create_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new CreateAccountRequest()
+        {
+            Email = "email@example.com",
+            Password = "password",
+            Name = "name"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account")
+            .ExpectedHeaders()
+            .WithJsonContent(request)
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteClient.Account.Create(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
---
+++ tests/PinguApps.Appwrite.Client.Tests/Clients/Account/AccountClientTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PinguApps.Appwrite.Shared.Tests;
+using Refit;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Client.Tests.Clients.Account;
+public partial class AccountClientTests
+{
+    private readonly MockHttpMessageHandler _mockHttp;
+    private readonly IAppwriteClient _appwriteClient;
+
+    public AccountClientTests()
+    {
+        _mockHttp = new MockHttpMessageHandler();
+        var services = new ServiceCollection();
+
+        services.AddAppwriteClientForServer("PROJECT_ID", Constants.Endpoint, new RefitSettings
+        {
+            HttpMessageHandlerFactory = () => _mockHttp
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        _appwriteClient = serviceProvider.GetRequiredService<IAppwriteClient>();
+    }
+}
+
+public static class AccountTestsExtensions
+{
+    public static MockedRequest ExpectedHeaders(this MockedRequest request, bool addSessionHeaders = false)
+    {
+        var req = request
+            .WithHeaders("x-appwrite-project", Constants.ProjectId)
+            .WithHeaders("x-sdk-name", Constants.SdkName)
+            .WithHeaders("x-sdk-platform", "client")
+            .WithHeaders("x-sdk-language", Constants.SdkLanguage)
+            .WithHeaders("x-sdk-version", Constants.SdkVersion)
+            .WithHeaders("x-appwrite-response-format", Constants.AppwriteResponseFormat);
+
+        if (addSessionHeaders)
+            return req.ExpectSessionHeaders();
+
+        return req;
+    }
+
+    public static MockedRequest ExpectSessionHeaders(this MockedRequest request)
+    {
+        return request
+            .WithHeaders("x-appwrite-session", Constants.Session);
+    }
+}
