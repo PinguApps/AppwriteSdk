f9a29c6304e4de97e36262ae1a8583fd20c1922a
Matthew Parker <pingu@pinguapps.com>
2024-07-07 04:58:46 +0100

    added intellisense to create account request

--- src/PinguApps.Appwrite.Common/Requests/CreateAccountRequest.cs
+++ src/PinguApps.Appwrite.Common/Requests/CreateAccountRequest.cs
@@ -2,17 +2,33 @@
 using PinguApps.Appwrite.Shared.Utils;
 
 namespace PinguApps.Appwrite.Shared.Requests;
+
+/// <summary>
+/// The request for creating an account
+/// </summary>
 public class CreateAccountRequest
 {
+    /// <summary>
+    /// User ID. Choose a custom ID or generate a random ID with <see cref="IdUtils.GenerateUniqueId(int)"/>. Valid chars are a-z, A-Z, 0-9, period, hyphen, and underscore. Can't start with a special char. Max length is 36 chars
+    /// </summary>
     [JsonPropertyName("userId")]
     public string UserId { get; set; } = IdUtils.GenerateUniqueId();
 
+    /// <summary>
+    /// User email
+    /// </summary>
     [JsonPropertyName("email")]
     public string Email { get; set; } = string.Empty;
 
+    /// <summary>
+    /// New user password. Must be between 8 and 256 chars
+    /// </summary>
     [JsonPropertyName("password")]
     public string Password { get; set; } = string.Empty;
 
+    /// <summary>
+    /// User name. Max length: 128 chars
+    /// </summary>
     [JsonPropertyName("name")]
     public string? Name { get; set; }
 }
