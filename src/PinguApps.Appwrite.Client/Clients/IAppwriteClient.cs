80b8159ab18cd2608d0bc1b14b07bee037f4526c
Matthew Parker <pingu@pinguapps.com>
2024-07-07 04:18:46 +0100

    added intellisense to IAppwriteClient

--- src/PinguApps.Appwrite.Client/Clients/IAppwriteClient.cs
+++ src/PinguApps.Appwrite.Client/Clients/IAppwriteClient.cs
@@ -1,8 +1,19 @@
 ﻿namespace PinguApps.Appwrite.Client;
 
+/// <summary>
+/// The root of the Client SDK. Access all API sections from here
+/// </summary>
 public interface IAppwriteClient
 {
+    /// <summary>
+    /// The Account API.
+    /// <para><see href="https://appwrite.io/docs/references/1.5.x/client-rest/account">Appwrite Docs</see></para>
+    /// </summary>
     IAccountClient Account { get; }
 
+    /// <summary>
+    /// Set the session of your logged in user
+    /// </summary>
+    /// <param name="session">The session token</param>
     void SetSession(string? session);
 }
