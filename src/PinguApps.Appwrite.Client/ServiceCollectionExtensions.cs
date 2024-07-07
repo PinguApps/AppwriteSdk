79aee373f43ba3523c61458fbcb6e7849ca5c63f
Matthew Parker <pingu@pinguapps.com>
2024-07-07 03:49:27 +0100

    Added intellisense to service collection extensions in client

--- src/PinguApps.Appwrite.Client/ServiceCollectionExtensions.cs
+++ src/PinguApps.Appwrite.Client/ServiceCollectionExtensions.cs
@@ -7,6 +7,14 @@ using Refit;
 namespace PinguApps.Appwrite.Client;
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds all necessary components for the Client SDK to work as a singleton. Best used on client-side
+    /// </summary>
+    /// <param name="services">The service collection to add to</param>
+    /// <param name="projectId">Your Appwrite Project ID</param>
+    /// <param name="endpoint">Your Appwrite Endpoint. Defaults to the cloud endpoint.</param>
+    /// <param name="refitSettings">Custom refit settings to customise the SDK.</param>
+    /// <returns>The service collection, enabling chaining</returns>
     public static IServiceCollection AddAppwriteClient(this IServiceCollection services, string projectId, string endpoint = "https://cloud.appwrite.io/v1", RefitSettings? refitSettings = null)
     {
         services.AddSingleton(sp => new HeaderHandler(projectId));
@@ -21,6 +29,14 @@ public static class ServiceCollectionExtensions
         return services;
     }
 
+    /// <summary>
+    /// Adds all necessary components for the Client SDK in a transient state. Best used on server-side to perform client SDK abilities on behalf of users
+    /// </summary>
+    /// <param name="services">The service collection to add to</param>
+    /// <param name="projectId">Your Appwrite Project ID</param>
+    /// <param name="endpoint">Your Appwrite Endpoint. Defaults to the could endpoint.</param>
+    /// <param name="refitSettings">Custom refit settings to customise the SDK.</param>
+    /// <returns>The service collection, enabling chaining</returns>
     public static IServiceCollection AddAppwriteClientForServer(this IServiceCollection services, string projectId, string endpoint = "https://cloud.appwrite.io/v1", RefitSettings? refitSettings = null)
     {
         services.AddSingleton(sp => new HeaderHandler(projectId));
