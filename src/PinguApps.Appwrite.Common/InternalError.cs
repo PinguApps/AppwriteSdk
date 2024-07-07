7f8a6b968a9928a255357e386435fe5367a3b883
Matthew Parker <pingu@pinguapps.com>
2024-07-07 04:39:50 +0100

    added intellisense to internalerror

--- src/PinguApps.Appwrite.Common/InternalError.cs
+++ src/PinguApps.Appwrite.Common/InternalError.cs
@@ -1,4 +1,9 @@
 ﻿namespace PinguApps.Appwrite.Shared;
+
+/// <summary>
+/// An internal error, indicating a fault within the SDK rather than Appwrite
+/// </summary>
+/// <param name="Message">The message of any thrown exception</param>
 public record InternalError(
     string Message
 );
