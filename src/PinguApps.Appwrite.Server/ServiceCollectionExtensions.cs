4287dd68512514248f4913bfe5345a4be136f2c7
Matthew Parker <pingu@pinguapps.com>
2024-07-07 04:28:33 +0100

    added server service intellisense

--- src/PinguApps.Appwrite.Server/ServiceCollectionExtensions.cs
+++ src/PinguApps.Appwrite.Server/ServiceCollectionExtensions.cs
@@ -6,8 +6,21 @@ using PinguApps.Appwrite.Server.Servers;
 using Refit;
 
 namespace PinguApps.Appwrite.Server;
+
+/// <summary>
+/// Provides extenions to IServiceCollection, to enable adding the SDK to your DI container
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds all necessary components for the Server SDK
+    /// </summary>
+    /// <param name="services">The service collection to add to</param>
+    /// <param name="projectId">Your Appwrite Project ID</param>
+    /// <param name="apiKey">Your Appwrite Api Key</param>
+    /// <param name="endpoint">Your Appwrite Endpoint. Defaults to the cloud endpoint.</param>
+    /// <param name="refitSettings">Custom refit settings to customise the SDK.</param>
+    /// <returns>The service collection, enabling chaining</returns>
     public static IServiceCollection AddAppwriteServer(this IServiceCollection services, string projectId, string apiKey, string endpoint = "https://cloud.appwrite.io/v1", RefitSettings? refitSettings = null)
     {
         services.AddSingleton(sp => new HeaderHandler(projectId, apiKey));
