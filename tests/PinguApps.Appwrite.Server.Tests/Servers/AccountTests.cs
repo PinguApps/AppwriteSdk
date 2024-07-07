9d98901d1e37dff01ca52d7caebb1f2c68034a0d
Matthew Parker <pingu@pinguapps.com>
2024-07-07 01:11:36 +0100

    added account tests exception handling

--- tests/PinguApps.Appwrite.Server.Tests/Servers/AccountTests.cs
+++ tests/PinguApps.Appwrite.Server.Tests/Servers/AccountTests.cs
@@ -73,6 +73,31 @@ public class AccountTests
         Assert.True(result.IsError);
         Assert.True(result.IsAppwriteError);
     }
+
+    [Fact]
+    public async Task Create_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new CreateAccountRequest()
+        {
+            Email = "email@example.com",
+            Password = "password",
+            Name = "name"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account")
+            .ExpectedHeaders()
+            .WithJsonContent(request)
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteServer.Account.Create(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
 }
 
 public static class AccountTestsExtensions
