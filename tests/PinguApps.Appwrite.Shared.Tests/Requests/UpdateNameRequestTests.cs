242427939925c8fead736a7c4e2a5f479d2f27ae
Matthew Parker <pingu@pinguapps.com>
2024-07-08 03:19:33 +0100

    Added shared tests

---
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/UpdateNameRequestTests.cs
@@ -0,0 +1,31 @@
+﻿using PinguApps.Appwrite.Shared.Requests;
+
+namespace PinguApps.Appwrite.Shared.Tests.Requests;
+public class UpdateNameRequestTests
+{
+    [Fact]
+    public void Constructor_InitializesWithExpectedValues()
+    {
+        // Arrange & Act
+        var request = new UpdateNameRequest();
+
+        // Assert
+        Assert.NotNull(request.Name);
+        Assert.Equal(string.Empty, request.Name);
+    }
+
+    [Theory]
+    [InlineData("name1")]
+    [InlineData("name2")]
+    public void Properties_CanBeSet(string name)
+    {
+        // Arrange
+        var request = new UpdateNameRequest();
+
+        // Act
+        request.Name = name;
+
+        // Assert
+        Assert.Equal(name, request.Name);
+    }
+}
