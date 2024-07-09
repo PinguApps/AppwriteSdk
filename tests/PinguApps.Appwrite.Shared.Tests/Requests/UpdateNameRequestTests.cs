abaab4eb85d29efa31b5fb9081a73b78bd97c23d
Matthew Parker <pingu@pinguapps.com>
2024-07-09 22:42:36 +0100

    Added tests to UpdateNameRequest

--- tests/PinguApps.Appwrite.Shared.Tests/Requests/UpdateNameRequestTests.cs
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/UpdateNameRequestTests.cs
@@ -1,4 +1,5 @@
-﻿using PinguApps.Appwrite.Shared.Requests;
+﻿using FluentValidation;
+using PinguApps.Appwrite.Shared.Requests;
 
 namespace PinguApps.Appwrite.Shared.Tests.Requests;
 public class UpdateNameRequestTests
@@ -27,5 +28,69 @@ public class UpdateNameRequestTests
 
         // Assert
         Assert.Equal(name, request.Name);
+    }
+
+    [Theory]
+    [InlineData("John")]
+    [InlineData("John Smith")]
+    public void IsValid_WithValidData_ReturnsTrue(string name)
+    {
+        // Arrange
+        var request = new UpdateNameRequest
+        {
+            Name = name
+        };
+
+        // Act
+        var isValid = request.IsValid();
+
+        // Assert
+        Assert.True(isValid);
+    }
+
+    [Theory]
+    [InlineData("")]
+    public void IsValid_WithInvalidData_ReturnsFalse(string name)
+    {
+        // Arrange
+        var request = new UpdateNameRequest
+        {
+            Name = name
+        };
+
+        // Act
+        var isValid = request.IsValid();
+
+        // Assert
+        Assert.False(isValid);
+    }
+
+    [Fact]
+    public void Validate_WithThrowOnFailuresTrue_ThrowsValidationExceptionOnFailure()
+    {
+        // Arrange
+        var request = new UpdateNameRequest
+        {
+            Name = ""
+        };
+
+        // Assert
+        Assert.Throws<ValidationException>(() => request.Validate(true));
+    }
+
+    [Fact]
+    public void Validate_WithThrowOnFailuresFalse_ReturnsInvalidResultOnFailure()
+    {
+        // Arrange
+        var request = new UpdateNameRequest
+        {
+            Name = ""
+        };
+
+        // Act
+        var result = request.Validate(false);
+
+        // Assert
+        Assert.False(result.IsValid);
     }
 }
