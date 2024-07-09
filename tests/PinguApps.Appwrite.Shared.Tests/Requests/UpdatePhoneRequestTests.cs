58aa32632ef27feab7b220354151d74d9d2fd34b
Matthew Parker <pingu@pinguapps.com>
2024-07-09 03:57:09 +0100

    added full test coverage for additions

--- tests/PinguApps.Appwrite.Shared.Tests/Requests/UpdatePhoneRequestTests.cs
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/UpdatePhoneRequestTests.cs
@@ -1,4 +1,5 @@
-﻿using PinguApps.Appwrite.Shared.Requests;
+﻿using FluentValidation;
+using PinguApps.Appwrite.Shared.Requests;
 
 namespace PinguApps.Appwrite.Shared.Tests.Requests;
 
@@ -32,5 +33,74 @@ public class UpdatePhoneRequestTests
         // Assert
         Assert.Equal(password, request.Password);
         Assert.Equal(phone, request.Phone);
+    }
+
+    [Fact]
+    public void IsValid_WithValidPhoneAndPassword_ReturnsTrue()
+    {
+        // Arrange
+        var request = new UpdatePhoneRequest
+        {
+            Phone = "+16175551212",
+            Password = "password123"
+        };
+
+        // Act
+        bool isValid = request.IsValid();
+
+        // Assert
+        Assert.True(isValid);
+    }
+
+    [Theory]
+    [InlineData("", "password123")] // Empty phone
+    [InlineData("+16175551212", "")] // Empty password
+    [InlineData("16175551212", "password123")] // Invalid phone
+    [InlineData("+16175551212", "pass")] // Short password
+    public void IsValid_WithInvalidInputs_ReturnsFalse(string phone, string password)
+    {
+        // Arrange
+        var request = new UpdatePhoneRequest
+        {
+            Phone = phone,
+            Password = password
+        };
+
+        // Act
+        var isValid = request.IsValid();
+
+        // Assert
+        Assert.False(isValid);
+    }
+
+    [Fact]
+    public void Validate_WithThrowOnFailuresTrue_ThrowsValidationExceptionOnFailure()
+    {
+        // Arrange
+        var request = new UpdatePhoneRequest
+        {
+            Phone = "invalid",
+            Password = "short"
+        };
+
+        // Assert
+        Assert.Throws<ValidationException>(() => request.Validate(true));
+    }
+
+    [Fact]
+    public void Validate_WithThrowOnFailuresFalse_ReturnsInvalidResultOnFailure()
+    {
+        // Arrange
+        var request = new UpdatePhoneRequest
+        {
+            Phone = "invalid",
+            Password = "short"
+        };
+
+        // Act
+        var result = request.Validate(false);
+
+        // Assert
+        Assert.False(result.IsValid);
     }
 }
