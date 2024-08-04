58f39d77ae959c250c2e0a06b06ac6ea8cca0589
Matthew Parker <pingu@pinguapps.com>
2024-08-04 17:55:33 +0200

    added client tests

---
+++ tests/PinguApps.Appwrite.Client.Tests/Clients/Account/AccountClientTests.CreateJwt.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Tests;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Client.Tests.Clients.Account;
+public partial class AccountClientTests
+{
+    [Fact]
+    public async Task CreateJwt_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account/jwt")
+            .ExpectedHeaders(true)
+            .Respond(Constants.AppJson, Constants.JwtResponse);
+
+        _appwriteClient.SetSession(Constants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.CreateJwt();
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task CreateJwt_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account/jwt")
+            .ExpectedHeaders(true)
+            .Respond(HttpStatusCode.BadRequest, Constants.AppJson, Constants.AppwriteError);
+
+        _appwriteClient.SetSession(Constants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.CreateJwt();
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task CreateJwt_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account/jwt")
+            .ExpectedHeaders(true)
+            .Throw(new HttpRequestException("An error occurred"));
+
+        _appwriteClient.SetSession(Constants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.CreateJwt();
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
