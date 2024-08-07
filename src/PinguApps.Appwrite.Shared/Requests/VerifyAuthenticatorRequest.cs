050dd731dd11e511a84551a66507630a3ca53f86
Matthew Parker <pingu@pinguapps.com>
2024-08-07 04:16:23 +0100

    Fixed failing tests with explicit json prop name

--- src/PinguApps.Appwrite.Shared/Requests/VerifyAuthenticatorRequest.cs
+++ src/PinguApps.Appwrite.Shared/Requests/VerifyAuthenticatorRequest.cs
@@ -1,7 +1,9 @@
-﻿using PinguApps.Appwrite.Shared.Requests.Validators;
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Validators;
 
 namespace PinguApps.Appwrite.Shared.Requests;
 public class VerifyAuthenticatorRequest : BaseRequest<VerifyAuthenticatorRequest, VerifyAuthenticatorRequestValidator>
 {
+    [JsonPropertyName("otp")]
     public string Otp { get; set; } = string.Empty;
 }
