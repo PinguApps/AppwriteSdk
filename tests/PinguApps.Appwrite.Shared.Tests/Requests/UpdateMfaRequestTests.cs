79c73077106708e252db67ba20c97d5bfd60ae87
Matthew Parker <pingu@pinguapps.com>
2024-08-08 23:46:07 +0100

    added shared tests

---
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/UpdateMfaRequestTests.cs
@@ -0,0 +1,47 @@
+﻿using PinguApps.Appwrite.Shared.Requests;
+
+namespace PinguApps.Appwrite.Shared.Tests.Requests;
+public class UpdateMfaRequestTests
+{
+
+    [Fact]
+    public void Constructor_InitializesWithExpectedValues()
+    {
+        // Arrange & Act
+        var request = new UpdateMfaRequest();
+
+        // Assert
+        Assert.False(request.MfaEnabled);
+    }
+
+    [Fact]
+    public void Properties_CanBeSet()
+    {
+        // Arrange
+        var request = new UpdateMfaRequest();
+
+        // Act
+        request.MfaEnabled = true;
+
+        // Assert
+        Assert.True(request.MfaEnabled);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void IsValid_WithValidData_ReturnsTrue(bool mfaEnabled)
+    {
+        // Arrange
+        var request = new UpdateMfaRequest
+        {
+            MfaEnabled = mfaEnabled
+        };
+
+        // Act
+        var isValid = request.IsValid();
+
+        // Assert
+        Assert.True(isValid);
+    }
+}
