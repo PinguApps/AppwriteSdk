772b4723596b1180fdf2baec32ee6c3c1b8a46fb
Matthew Parker <pingu@pinguapps.com>
2024-08-11 13:54:46 +0100

    added client tests

---
+++ tests/PinguApps.Appwrite.Client.Tests/Clients/Account/AccountClientTests.DeleteSessions.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Client.Clients;
+using PinguApps.Appwrite.Shared.Tests;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Client.Tests.Clients.Account;
+public partial class AccountClientTests
+{
+    [Fact]
+    public async Task DeleteSessions_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Delete, $"{Constants.Endpoint}/account/sessions")
+            .ExpectedHeaders(true)
+            .Respond(HttpStatusCode.NoContent);
+
+        _appwriteClient.SetSession(Constants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.DeleteSessions();
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task DeleteSessions_ShouldReturnError_WhenSessionIsNull()
+    {
+        // Act
+        var result = await _appwriteClient.Account.DeleteSessions();
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsInternalError);
+        Assert.Equal(ISessionAware.SessionExceptionMessage, result.Result.AsT2.Message);
+    }
+
+    [Fact]
+    public async Task DeleteSessions_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Delete, $"{Constants.Endpoint}/account/sessions")
+            .ExpectedHeaders(true)
+            .Respond(HttpStatusCode.BadRequest, Constants.AppJson, Constants.AppwriteError);
+
+        _appwriteClient.SetSession(Constants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.DeleteSessions();
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task DeleteSessions_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Delete, $"{Constants.Endpoint}/account/sessions")
+            .ExpectedHeaders(true)
+            .Throw(new HttpRequestException("An error occurred"));
+
+        _appwriteClient.SetSession(Constants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.DeleteSessions();
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
