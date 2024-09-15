4853cf469c068b8b7426ed18deb58bf559305b81
Matthew Parker <pingu@pinguapps.com>
2024-09-15 17:17:36 +0100

    implemented delete session on client

--- src/PinguApps.Appwrite.Shared/Requests/DeleteSessionRequest.cs
+++ src/PinguApps.Appwrite.Shared/Requests/DeleteSessionRequest.cs
@@ -1,11 +1,12 @@
 ﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Validators;
 
 namespace PinguApps.Appwrite.Shared.Requests;
 
 /// <summary>
 /// The request for deleting a session (logging out of current session)
 /// </summary>
-public class DeleteSessionRequest
+public class DeleteSessionRequest : BaseRequest<DeleteSessionRequest, DeleteSessionRequestValidator>
 {
     /// <summary>
     /// Session ID. Use the string 'current' to delete the current device session.
