733597e0566fde1d2c9d054248d79d0269acb92d
Matthew Parker <pingu@pinguapps.com>
2024-09-22 16:38:25 +0100

    Added request for update magic url session

---
+++ src/PinguApps.Appwrite.Shared/Requests/UpdateMagicUrlSessionRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PinguApps.Appwrite.Shared.Requests;
+public class UpdateMagicUrlSessionRequest : BaseRequest<UpdateMagicUrlSessionRequest, UpdateMagicUrlSessionRequestValidator>
+{
+    /// <summary>
+    /// User ID. Choose a custom ID or generate a random ID with <see cref="Utils.IdUtils.GenerateUniqueId(int)"/>. Valid chars are a-z, A-Z, 0-9, period, hyphen, and underscore. Can't start with a special char. Max length is 36 chars.
+    /// </summary>
+    [JsonPropertyName("userId")]
+    public string UserId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Valid verification token.
+    /// </summary>
+    [JsonPropertyName("secret")]
+    public string Secret { get; set; } = string.Empty;
+}
