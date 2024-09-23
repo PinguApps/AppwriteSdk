f1005301d0b4cda6d68ddbf00c945e9febd1b535
Matthew Parker <pingu@pinguapps.com>
2024-09-23 04:16:55 +0100

    added shared tests

---
+++ tests/PinguApps.Appwrite.Shared.Tests/Responses/CreateOauth2TokenTests.cs
@@ -0,0 +1,18 @@
+﻿using PinguApps.Appwrite.Shared.Responses;
+
+namespace PinguApps.Appwrite.Shared.Tests.Responses;
+public class CreateOauth2TokenTests
+{
+    [Fact]
+    public void Constructor_AssignsPropertiesCorrectly()
+    {
+        // Arrange
+        var uri = "https://example.com";
+
+        // Act
+        var response = new CreateOauth2Token(uri);
+
+        // Assert
+        Assert.Equal(uri, response.Uri);
+    }
+}
