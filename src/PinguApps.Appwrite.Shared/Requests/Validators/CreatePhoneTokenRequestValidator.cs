a6f274a494a17eed3559dd865915452490d34e38
Matthew Parker <pingu@pinguapps.com>
2024-09-23 23:29:37 +0100

    added shared models

---
+++ src/PinguApps.Appwrite.Shared/Requests/Validators/CreatePhoneTokenRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Validators;
+public class CreatePhoneTokenRequestValidator : AbstractValidator<CreatePhoneTokenRequest>
+{
+    public CreatePhoneTokenRequestValidator()
+    {
+
+        RuleFor(request => request.UserId)
+            .NotEmpty().WithMessage("User ID must not be empty.")
+            .Matches("^[a-zA-Z0-9][a-zA-Z0-9._-]{0,35}$").WithMessage("User ID must be alphanumeric and can include periods, hyphens, and underscores. It cannot start with a special character and must be at most 36 characters long.");
+
+        RuleFor(request => request.PhoneNumber)
+            .NotEmpty().WithMessage("Phone number must not be empty.")
+            .Matches(@"^\+\d{1,15}$").WithMessage("Phone number must start with a '+' and include the country code, followed by up to 15 digits.");
+    }
+}
