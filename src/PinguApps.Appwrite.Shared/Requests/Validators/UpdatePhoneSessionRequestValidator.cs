153e93e7a2d0a1e76f687c45536fc01bae0f6802
Matthew Parker <pingu@pinguapps.com>
2024-09-24 19:14:29 +0100

    added shared tests

--- src/PinguApps.Appwrite.Shared/Requests/Validators/UpdatePhoneSessionRequestValidator.cs
+++ src/PinguApps.Appwrite.Shared/Requests/Validators/UpdatePhoneSessionRequestValidator.cs
@@ -7,7 +7,7 @@ public class UpdatePhoneSessionRequestValidator : AbstractValidator<UpdatePhoneS
     {
         RuleFor(x => x.UserId)
             .NotEmpty().WithMessage("The user ID is required.")
-            .Matches(@"^[a-zA-Z0-9\.\-_]{1,36}$").WithMessage("The user ID must be between 1 and 36 characters long and can only contain a-z, A-Z, 0-9, period, hyphen, and underscore.");
+            .Matches("^[a-zA-Z0-9][a-zA-Z0-9._-]{0,35}$").WithMessage("The user ID must be between 1 and 36 characters long and can only contain a-z, A-Z, 0-9, period, hyphen, and underscore.");
 
         RuleFor(x => x.Secret)
             .NotEmpty().WithMessage("The secret is required.");
