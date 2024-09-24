e380e04185f0ef22d8d3aa623de596628feb9f6c
Matthew Parker <pingu@pinguapps.com>
2024-09-24 22:08:40 +0100

    added client tests

---
+++ tests/PinguApps.Appwrite.Client.Tests/Clients/Account/AccountClientTests.CreatePhoneVerification.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Client.Clients;
+using PinguApps.Appwrite.Shared.Tests;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Client.Tests.Clients.Account;
+public partial class AccountClientTests
+{
+    [Fact]
+    public async Task CreatePhoneVerification_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account/verification/phone")
+            .ExpectedHeaders(true)
+            .Respond(Constants.AppJson, Constants.TokenResponse);
+
+        _appwriteClient.SetSession(Constants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.CreatePhoneVerification();
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task CreatePhoneVerification_ShouldReturnError_WhenSessionIsNull()
+    {
+        // Act
+        var result = await _appwriteClient.Account.CreatePhoneVerification();
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsInternalError);
+        Assert.Equal(ISessionAware.SessionExceptionMessage, result.Result.AsT2.Message);
+    }
+
+    [Fact]
+    public async Task CreatePhoneVerification_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account/verification/phone")
+            .ExpectedHeaders(true)
+            .Respond(HttpStatusCode.BadRequest, Constants.AppJson, Constants.AppwriteError);
+
+        _appwriteClient.SetSession(Constants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.CreatePhoneVerification();
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task CreatePhoneVerification_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Post, $"{Constants.Endpoint}/account/verification/phone")
+            .ExpectedHeaders(true)
+            .Throw(new HttpRequestException("An error occurred"));
+
+        _appwriteClient.SetSession(Constants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.CreatePhoneVerification();
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
