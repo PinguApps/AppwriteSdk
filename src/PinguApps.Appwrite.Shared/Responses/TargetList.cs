ed1e1fb08e9bf6ecaca8360cc1f45ec63ea53e64
Matthew Parker <pingu@pinguapps.com>
2024-09-26 17:54:48 +0100

    Added TargetList response

---
+++ src/PinguApps.Appwrite.Shared/Responses/TargetList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PinguApps.Appwrite.Shared.Responses;
+
+/// <summary>
+/// An Appwrite Target List object
+/// </summary>
+/// <param name="Total">Total number of targets documents that matched your query</param>
+/// <param name="Targets">List of targets. Can be one of: <see cref="Target"/></param>
+public record TargetList(
+    [property: JsonPropertyName("total")] int Total,
+    [property: JsonPropertyName("targets")] IReadOnlyList<Target> Targets
+);
