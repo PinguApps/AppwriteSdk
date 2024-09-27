37e5abc08233f357b2e9573fb7f7da4e68e21131
Matthew Parker <pingu@pinguapps.com>
2024-09-27 23:59:10 +0100

    Added ListLogsRequest

---
+++ src/PinguApps.Appwrite.Shared/Requests/Account/Validators/ListLogsRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using PinguApps.Appwrite.Shared.Requests.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Account.Validators;
+public class ListLogsRequestValidator : AbstractValidator<ListLogsRequest>
+{
+    public ListLogsRequestValidator()
+    {
+        Include(new QueryBaseRequestValidator<ListLogsRequest, ListLogsRequestValidator>());
+    }
+}
