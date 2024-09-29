4f0c002e515024dba0e2ccaf5a36b8c7b6286cbc
Matthew Parker <pingu@pinguapps.com>
2024-09-29 06:36:28 +0100

    added request for update user labels

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/UpdateUserLabels.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class UpdateUserLabels : UserIdBaseRequest<UpdateUserLabels, UpdateUserLabelsValidator>
+{
+    /// <summary>
+    /// Array of user labels. Replaces the previous labels. Maximum of 1000 labels are allowed, each up to 36 alphanumeric characters long.
+    /// </summary>
+    [JsonPropertyName("labels")]
+    public List<string> Labels { get; set; } = [];
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/Validators/UpdateUserLabelsValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users.Validators;
+public class UpdateUserLabelsValidator : AbstractValidator<UpdateUserLabels>
+{
+    public UpdateUserLabelsValidator()
+    {
+        Include(new UserIdBaseRequestValidator<UpdateUserLabels, UpdateUserLabelsValidator>());
+
+        RuleFor(x => x.Labels)
+            .NotNull().WithMessage("Labels cannot be null.")
+            .Must(labels => labels.Count <= 1000).WithMessage("A maximum of 1000 labels are allowed.")
+            .ForEach(labelRule => labelRule
+                .NotEmpty().WithMessage("Label cannot be empty.")
+                .MaximumLength(36).WithMessage("Label must be at most 36 characters long.")
+                .Matches("^[a-zA-Z0-9]*$").WithMessage("Label must be alphanumeric.")
+            );
+    }
+}
