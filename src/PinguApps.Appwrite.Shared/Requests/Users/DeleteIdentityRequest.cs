1db30706157f37b675bbad579bd6710c5f54ad1e
Matthew Parker <pingu@pinguapps.com>
2024-09-29 04:33:42 +0100

    added delete identity request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/DeleteIdentityRequest.cs
@@ -0,0 +1,10 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class DeleteIdentityRequest : BaseRequest<DeleteIdentityRequest, DeleteIdentityRequestValidator>
+{
+    /// <summary>
+    /// Identity ID
+    /// </summary>
+    public string IdentityId { get; set; } = string.Empty;
+}
