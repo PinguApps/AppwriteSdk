51f3f969fd0da4911b7e34bd5329c747d92dc24a
Matthew Parker <pingu@pinguapps.com>
2024-09-29 05:58:37 +0100

    added request for get user

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/Validators/GetUserRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users.Validators;
+public class GetUserRequestValidator : AbstractValidator<GetUserRequest>
+{
+    public GetUserRequestValidator()
+    {
+        Include(new UserIdBaseRequestValidator<GetUserRequest, GetUserRequestValidator>());
+    }
+}
