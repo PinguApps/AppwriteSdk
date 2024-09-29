782df727277b5d4a6279785c03a64c0d60ff8267
Matthew Parker <pingu@pinguapps.com>
2024-09-29 05:47:06 +0100

    added create user with sha

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/CreateUserWithShaPasswordRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class CreateUserWithShaPasswordRequest : CreateUserWithPasswordBaseRequest<CreateUserWithShaPasswordRequest, CreateUserWithShaPasswordRequestValidator>
+{
+    /// <summary>
+    /// Optional SHA version used to hash password. Allowed values are: <c>sha1</c>, <c>sha224</c>, <c>sha256</c>, <c>sha384</c>, <c>sha512/224</c>, <c>sha512/256</c>, <c>sha512</c>, <c>sha3-224</c>, <c>sha3-256</c>, <c>sha3-384</c>, <c>sha3-512</c>
+    /// </summary>
+    [JsonPropertyName("passwordVersion")]
+    public string? PasswordVersion { get; set; }
+}
