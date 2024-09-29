e3ee3ed45e7c83830be94e544d5e668858b4e53a
Matthew Parker <pingu@pinguapps.com>
2024-09-29 04:56:23 +0100

    Added create user with md5

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/CreateUserWithMd5PasswordRequest.cs
@@ -0,0 +1,6 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class CreateUserWithMd5PasswordRequest : CreateUserWithPasswordBaseRequest<CreateUserWithMd5PasswordRequest, CreateUserWithMd5PasswordRequestValidator>
+{
+}
