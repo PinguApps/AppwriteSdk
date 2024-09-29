9a047b6f9a3ab28e1e984a4029b1c2f2253422bf
Matthew Parker <pingu@pinguapps.com>
2024-09-29 04:26:27 +0100

    added list identities request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/Validators/ListIdentitiesRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using PinguApps.Appwrite.Shared.Requests.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users.Validators;
+public class ListIdentitiesRequestValidator : AbstractValidator<ListIdentitiesRequest>
+{
+    public ListIdentitiesRequestValidator()
+    {
+        Include(new QuerySearchBaseRequestValidator<ListIdentitiesRequest, ListIdentitiesRequestValidator>());
+    }
+}
