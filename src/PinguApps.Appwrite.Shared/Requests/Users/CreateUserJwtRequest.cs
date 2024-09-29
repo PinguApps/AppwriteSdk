e439637233cfa71e4bb94566137316f557f08f49
Matthew Parker <pingu@pinguapps.com>
2024-09-29 06:25:00 +0100

    added create user jwt request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/CreateUserJwtRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class CreateUserJwtRequest : UserIdBaseRequest<CreateUserJwtRequest, CreateUserJwtRequestValidator>
+{
+    /// <summary>
+    /// Session ID. Use the string <c>recent</c> to use the most recent session. Defaults to the most recent session
+    /// </summary>
+    [JsonPropertyName("sessionId")]
+    public string? SessionId { get; set; }
+
+    /// <summary>
+    /// Time in seconds before JWT expires. Default duration is 900 seconds, and maximum is 3600 seconds
+    /// </summary>
+    [JsonPropertyName("duration")]
+    public int? Duration { get; set; }
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/Validators/CreateUserJwtRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users.Validators;
+public class CreateUserJwtRequestValidator : AbstractValidator<CreateUserJwtRequest>
+{
+    public CreateUserJwtRequestValidator()
+    {
+        Include(new UserIdBaseRequestValidator<CreateUserJwtRequest, CreateUserJwtRequestValidator>());
+
+        RuleFor(x => x.SessionId)
+            .NotEmpty()
+            .When(x => x.SessionId is not null)
+            .WithMessage("Session ID must be null or a non-empty string.");
+
+        RuleFor(x => x.Duration)
+            .GreaterThanOrEqualTo(0).WithMessage("Duration must be at least 0 seconds.")
+            .LessThanOrEqualTo(3600).WithMessage("Duration must be at most 3600 seconds.")
+            .When(x => x.Duration.HasValue);
+    }
+}
