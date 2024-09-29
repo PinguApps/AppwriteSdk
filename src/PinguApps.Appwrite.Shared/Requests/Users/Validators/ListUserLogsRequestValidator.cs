090971544cfc1955b7d9a5c3d6306e3ab0c41534
Matthew Parker <pingu@pinguapps.com>
2024-09-29 07:25:32 +0100

    added tests for list user logs

--- src/PinguApps.Appwrite.Shared/Requests/Users/Validators/ListUserLogsRequestValidator.cs
+++ src/PinguApps.Appwrite.Shared/Requests/Users/Validators/ListUserLogsRequestValidator.cs
@@ -8,11 +8,13 @@ public class ListUserLogsRequestValidator : AbstractValidator<ListUserLogsReques
         Include(new UserIdBaseRequestValidator<ListUserLogsRequest, ListUserLogsRequestValidator>());
 
         RuleFor(x => x.Queries)
-            .Must(queries => queries == null || queries.Count <= 100)
+            .Must(queries => queries is null || queries.Count <= 100)
             .WithMessage("A maximum of 100 queries are allowed.")
             .ForEach(queryRule => queryRule
                 .Must(query => query.GetQueryString().Length <= 4096)
                 .WithMessage("Each query must be at most 4096 characters long.")
+                .Must(x => x.Method == "limit" || x.Method == "offset")
+                .WithMessage("Only supported query methods are limit and offset")
             );
     }
 }
