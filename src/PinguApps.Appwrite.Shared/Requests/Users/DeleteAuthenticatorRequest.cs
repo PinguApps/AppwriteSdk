0543ab8649d142fd439044780449919c47447f4e
Matthew Parker <pingu@pinguapps.com>
2024-09-29 17:09:17 +0100

    added delete authenticator request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/DeleteAuthenticatorRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class DeleteAuthenticatorRequest : UserIdBaseRequest<DeleteAuthenticatorRequest, DeleteAuthenticatorRequestValidator>
+{
+    /// <summary>
+    /// Type of authenticator
+    /// </summary>
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = string.Empty;
+}
