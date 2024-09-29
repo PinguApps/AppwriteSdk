32c8733d786ea90519946fecfe33af4de086f563
Matthew Parker <pingu@pinguapps.com>
2024-09-29 17:18:30 +0100

    added request for get mfa recovery codes

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/GetMfaRecoveryCodesRequest.cs
@@ -0,0 +1,6 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class GetMfaRecoveryCodesRequest : UserIdBaseRequest<GetMfaRecoveryCodesRequest, GetMfaRecoveryCodesRequestValidator>
+{
+}
