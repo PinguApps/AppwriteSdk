9c0555e376d8f6a08e2f2aed4758f767e9ec9493
Matthew Parker <pingu@pinguapps.com>
2024-09-29 04:16:06 +0100

    Added tests for Create users with argon2 password

---
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/Users/CreateUserWithArgon2PasswordRequestTests.cs
@@ -0,0 +1,8 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Users;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Tests.Requests.Users;
+public class CreateUserWithArgon2PasswordRequestTests : CreateUserWithPasswordBaseRequestTests<CreateUserWithArgon2PasswordRequest, CreateUserWithArgon2PasswordRequestValidator>
+{
+    protected override CreateUserWithArgon2PasswordRequest CreateValidRequest => new();
+}
