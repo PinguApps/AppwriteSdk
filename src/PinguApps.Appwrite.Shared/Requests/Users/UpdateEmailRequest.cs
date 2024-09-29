d72a6f480b6983b300b20213d174df6d0d4121fd
Matthew Parker <pingu@pinguapps.com>
2024-09-29 06:11:33 +0100

    added update email request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/UpdateEmailRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class UpdateEmailRequest : UserIdBaseRequest<UpdateEmailRequest, UpdateEmailRequestValidator>
+{
+    /// <summary>
+    /// User email
+    /// </summary>
+    [JsonPropertyName("email")]
+    public string Email { get; set; } = string.Empty;
+}
