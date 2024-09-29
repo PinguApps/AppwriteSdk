e2e728fbe885b9c48001b57f3be1216a98fd111a
Matthew Parker <pingu@pinguapps.com>
2024-09-29 07:32:01 +0100

    added update mfa request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/UpdateMfaRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class UpdateMfaRequest : UserIdBaseRequest<UpdateMfaRequest, UpdateMfaRequestValidator>
+{
+    /// <summary>
+    /// Enable or disable MFA on a user account
+    /// </summary>
+    [JsonPropertyName("mfa")]
+    public bool Mfa { get; set; }
+}
