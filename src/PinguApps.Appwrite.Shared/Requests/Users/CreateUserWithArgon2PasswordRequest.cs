e2dcf55b60c25a4ee0c4308599c3d89c5cabe723
Matthew Parker <pingu@pinguapps.com>
2024-09-29 04:13:34 +0100

    added CreateUserWithArgon2PasswordRequest

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/CreateUserWithArgon2PasswordRequest.cs
@@ -0,0 +1,6 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class CreateUserWithArgon2PasswordRequest : CreateUserWithPasswordBaseRequest<CreateUserWithArgon2PasswordRequest, CreateUserWithArgon2PasswordRequestValidator>
+{
+}
