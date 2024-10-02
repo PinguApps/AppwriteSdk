7c6c0660ccce634f42d629f7ff41ddeed57f3945
Matthew Parker <pingu@pinguapps.com>
2024-10-02 02:11:12 +0100

    added update phone

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/UpdatePhoneRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class UpdatePhoneRequest : UserIdBaseRequest<UpdatePhoneRequest, UpdatePhoneRequestValidator>
+{
+    /// <summary>
+    /// User phone number. Format this number with a leading '+' and a country code, e.g., +16175551212
+    /// </summary>
+    [JsonPropertyName("number")]
+    public string PhoneNumber { get; set; } = string.Empty;
+}
