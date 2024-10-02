12027a19bfb76f2aabb906e5456f945cbd2cd175
Matthew Parker <pingu@pinguapps.com>
2024-10-02 02:21:56 +0100

    added update user preferences

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/UpdateUserPreferencesRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class UpdateUserPreferencesRequest : UserIdBaseRequest<UpdateUserPreferencesRequest, UpdateUserPreferencesRequestValidator>
+{
+    /// <summary>
+    /// Prefs key-value JSON object
+    /// </summary>
+    [JsonPropertyName("prefs")]
+    public IDictionary<string, string> Preferences { get; set; } = new Dictionary<string, string>();
+}
