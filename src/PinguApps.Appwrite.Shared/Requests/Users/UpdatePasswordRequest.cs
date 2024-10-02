672508b006af6fba934d44b89bc566ed7e58420f
Matthew Parker <pingu@pinguapps.com>
2024-10-02 02:05:34 +0100

    added update apssword request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/UpdatePasswordRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class UpdatePasswordRequest : UserIdBaseRequest<UpdatePasswordRequest, UpdatePasswordRequestValidator>
+{
+    /// <summary>
+    /// New user password. Must be at least 8 chars
+    /// </summary>
+    [JsonPropertyName("password")]
+    public string Password { get; set; } = string.Empty;
+}
