3b184cdd986b800c2115e1e6a72efb3d2ab566dd
Matthew Parker <pingu@pinguapps.com>
2024-10-03 17:31:40 +0100

    added request for update user status

--- src/PinguApps.Appwrite.Shared/Requests/Users/DeleteUserSessionRequest.cs
+++ src/PinguApps.Appwrite.Shared/Requests/Users/DeleteUserSessionRequest.cs
@@ -1,7 +1,12 @@
-﻿using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
 
 namespace PinguApps.Appwrite.Shared.Requests.Users;
 public class DeleteUserSessionRequest : UserIdBaseRequest<DeleteUserSessionRequest, DeleteUserSessionRequestValidator>
 {
+    /// <summary>
+    /// Session Id
+    /// </summary>
+    [JsonPropertyName("sessionId")]
     public string SessionId { get; set; } = string.Empty;
 }
---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/UpdateUserStatusRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class UpdateUserStatusRequest : UserIdBaseRequest<UpdateUserStatusRequest, UpdateUserStatusRequestValidator>
+{
+    /// <summary>
+    /// User Status. To activate the user pass <c>true</c> and to block the user pass <c>false</c>.
+    /// </summary>
+    [JsonPropertyName("status")]
+    public bool Status { get; set; }
+}
