f673e4dd963fac70187b14e90541a15446f07fc3
Matthew Parker <pingu@pinguapps.com>
2024-10-03 19:58:22 +0100

    Added Get User  Target

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/GetUserTargetRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class GetUserTargetRequest : UserIdBaseRequest<GetUserTargetRequest, GetUserTargetRequestValidator>
+{
+    /// <summary>
+    /// Target ID
+    /// </summary>
+    [JsonPropertyName("targetId")]
+    public string TargetId { get; set; } = string.Empty;
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/Validators/GetUserTargetRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users.Validators;
+public class GetUserTargetRequestValidator : AbstractValidator<GetUserTargetRequest>
+{
+    public GetUserTargetRequestValidator()
+    {
+        Include(new UserIdBaseRequestValidator<GetUserTargetRequest, GetUserTargetRequestValidator>());
+
+        RuleFor(x => x.TargetId)
+            .NotEmpty()
+            .WithMessage("TargetId is required.");
+    }
+}
