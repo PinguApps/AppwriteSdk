7c8bb8abcfab6e875fbeea74441dd96383eeb4dd
Matthew Parker <pingu@pinguapps.com>
2024-10-03 17:25:10 +0100

    added request for delete user session

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/DeleteUserSessionRequest.cs
@@ -0,0 +1,7 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class DeleteUserSessionRequest : UserIdBaseRequest<DeleteUserSessionRequest, DeleteUserSessionRequestValidator>
+{
+    public string SessionId { get; set; } = string.Empty;
+}
