b3bb4245f994d9a14845507cbd1da057cb13c3e6
Matthew Parker <pingu@pinguapps.com>
2024-10-05 09:21:21 +0100

    added delete user target request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/DeleteUserTargetRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class DeleteUserTargetRequest : UserIdBaseRequest<DeleteUserTargetRequest, DeleteUserTargetRequestValidator>
+{
+    /// <summary>
+    /// Target ID
+    /// </summary>
+    [JsonPropertyName("targetId")]
+    public string TargetId { get; set; } = string.Empty;
+}
