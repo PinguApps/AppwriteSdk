c2614cc9b3fa0bd4ff377f519f574ea68f0a78ee
Matthew Parker <pingu@pinguapps.com>
2024-10-05 09:34:29 +0100

    added request for update email veirfication

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/UpdateEmailVerificationRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class UpdateEmailVerificationRequest : UserIdBaseRequest<UpdateEmailVerificationRequest, UpdateEmailVerificationRequestValidator>
+{
+    /// <summary>
+    /// User email verification status
+    /// </summary>
+    [JsonPropertyName("emailVerification")]
+    public bool EmailVerification { get; set; }
+}
