f805fc8c057e5c454f10551ca795de53fd8f09d0
Matthew Parker <pingu@pinguapps.com>
2024-10-05 09:41:14 +0100

    added requests for update phone verification

---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/UpdatePhoneVerificationRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Users.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users;
+public class UpdatePhoneVerificationRequest : UserIdBaseRequest<UpdatePhoneVerificationRequest, UpdatePhoneVerificationRequestValidator>
+{
+    /// <summary>
+    /// User phone verification status
+    /// </summary>
+    [JsonPropertyName("phoneVerification")]
+    public bool PhoneVerification { get; set; }
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Users/Validators/UpdatePhoneVerificationRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Users.Validators;
+public class UpdatePhoneVerificationRequestValidator : AbstractValidator<UpdatePhoneVerificationRequest>
+{
+    public UpdatePhoneVerificationRequestValidator()
+    {
+        Include(new UserIdBaseRequestValidator<UpdatePhoneVerificationRequest, UpdatePhoneVerificationRequestValidator>());
+
+        RuleFor(x => x.PhoneVerification)
+            .NotNull()
+            .WithMessage("Phone verification status is required.");
+    }
+}
