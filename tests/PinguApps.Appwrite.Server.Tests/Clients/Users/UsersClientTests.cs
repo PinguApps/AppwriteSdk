df6dd412f4cfa79d56b6fd143e53a5536565fe47
Matthew Parker <pingu@pinguapps.com>
2024-10-05 23:06:21 +0100

    added tests for List Users

---
+++ tests/PinguApps.Appwrite.Server.Tests/Clients/Users/UsersClientTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PinguApps.Appwrite.Server.Clients;
+using PinguApps.Appwrite.Shared.Tests;
+using Refit;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Server.Tests.Clients.Users;
+public partial class UsersClientTests
+{
+    private readonly MockHttpMessageHandler _mockHttp;
+    private readonly IAppwriteClient _appwriteClient;
+
+    public UsersClientTests()
+    {
+        _mockHttp = new MockHttpMessageHandler();
+        var services = new ServiceCollection();
+
+        services.AddAppwriteServer(Constants.ProjectId, Constants.ApiKey, Constants.Endpoint, new RefitSettings
+        {
+            HttpMessageHandlerFactory = () => _mockHttp
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        _appwriteClient = serviceProvider.GetRequiredService<IAppwriteClient>();
+    }
+}
