a62e9436492474bb2f5efa094b7e956ace1120c5
Matthew Parker <pingu@pinguapps.com>
2024-10-08 19:21:33 +0100

    implemented update user labels

--- src/PinguApps.Appwrite.Playground/App.cs
+++ src/PinguApps.Appwrite.Playground/App.cs
@@ -17,13 +17,13 @@ internal class App
 
     public async Task Run(string[] args)
     {
-        var request = new UpdateEmailRequest()
+        var request = new UpdateUserLabelsRequest()
         {
             UserId = "664aac1a00113f82e620",
-            Email = "test@example.com"
+            Labels = ["test", "admin"]
         };
 
-        var response = await _server.Users.UpdateEmail(request);
+        var response = await _server.Users.UpdateUserLabels(request);
 
         Console.WriteLine(response.Result.Match(
             result => result.ToString(),
