4ede33fa19c23f88bb2c6443f76e795280f02f83
Matthew Parker <pingu@pinguapps.com>
2024-10-08 19:14:04 +0100

    added tests for update email

---
+++ tests/PinguApps.Appwrite.Server.Tests/Clients/Users/UsersClientTests.UpdateEmail.cs
@@ -0,0 +1,79 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Requests.Users;
+using PinguApps.Appwrite.Shared.Tests;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Server.Tests.Clients.Users;
+public partial class UsersClientTests
+{
+    [Theory]
+    [InlineData("user123", "newemail@example.com")]
+    [InlineData("user456", "anotheremail@example.com")]
+    public async Task UpdateEmail_ShouldReturnSuccess_WhenApiCallSucceeds(string userId, string newEmail)
+    {
+        // Arrange
+        var request = new UpdateEmailRequest
+        {
+            UserId = userId,
+            Email = newEmail
+        };
+
+        _mockHttp.Expect(HttpMethod.Patch, $"{Constants.Endpoint}/users/{userId}/email")
+            .WithJsonContent(request)
+            .ExpectedHeaders()
+            .Respond(Constants.AppJson, Constants.UserResponse);
+
+        // Act
+        var result = await _appwriteClient.Users.UpdateEmail(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task UpdateEmail_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new UpdateEmailRequest
+        {
+            UserId = "user123",
+            Email = "newemail@example.com"
+        };
+
+        _mockHttp.Expect(HttpMethod.Patch, $"{Constants.Endpoint}/users/user123/email")
+            .WithJsonContent(request)
+            .ExpectedHeaders()
+            .Respond(HttpStatusCode.BadRequest, Constants.AppJson, Constants.AppwriteError);
+
+        // Act
+        var result = await _appwriteClient.Users.UpdateEmail(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task UpdateEmail_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new UpdateEmailRequest
+        {
+            UserId = "user123",
+            Email = "newemail@example.com"
+        };
+
+        _mockHttp.Expect(HttpMethod.Patch, $"{Constants.Endpoint}/users/user123/email")
+            .WithJsonContent(request)
+            .ExpectedHeaders()
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteClient.Users.UpdateEmail(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
