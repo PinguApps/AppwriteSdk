299169127101948aa53268ea3c1ad45983af2b10
Matthew Parker <pingu@pinguapps.com>
2024-10-09 01:29:03 +0100

    added tests for get mfa recovery codes

---
+++ tests/PinguApps.Appwrite.Server.Tests/Clients/Users/UsersClientTests.GetMfaRecoveryCodes.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Requests.Users;
+using PinguApps.Appwrite.Shared.Tests;
+using PinguApps.Appwrite.Shared.Utils;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Server.Tests.Clients.Users;
+public partial class UsersClientTests
+{
+    [Fact]
+    public async Task GetMfaRecoveryCodes_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        var request = new GetMfaRecoveryCodesRequest
+        {
+            UserId = IdUtils.GenerateUniqueId()
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{Constants.Endpoint}/users/{request.UserId}/mfa/recovery-codes")
+            .ExpectedHeaders()
+            .Respond(Constants.AppJson, Constants.MfaRecoveryCodesResponse);
+
+        // Act
+        var result = await _appwriteClient.Users.GetMfaRecoveryCodes(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task GetMfaRecoveryCodes_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new GetMfaRecoveryCodesRequest
+        {
+            UserId = IdUtils.GenerateUniqueId()
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{Constants.Endpoint}/users/{request.UserId}/mfa/recovery-codes")
+            .ExpectedHeaders()
+            .Respond(HttpStatusCode.BadRequest, Constants.AppJson, Constants.AppwriteError);
+
+        // Act
+        var result = await _appwriteClient.Users.GetMfaRecoveryCodes(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task GetMfaRecoveryCodes_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new GetMfaRecoveryCodesRequest
+        {
+            UserId = IdUtils.GenerateUniqueId()
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{Constants.Endpoint}/users/{request.UserId}/mfa/recovery-codes")
+            .ExpectedHeaders()
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteClient.Users.GetMfaRecoveryCodes(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
