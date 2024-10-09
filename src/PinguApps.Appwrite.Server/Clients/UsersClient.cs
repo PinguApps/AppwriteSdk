2698d92b12829d10be19762fc3b361a5ec5d4d52
Matthew Parker <pingu@pinguapps.com>
2024-10-09 22:17:18 +0100

    implemented update password

--- src/PinguApps.Appwrite.Server/Clients/UsersClient.cs
+++ src/PinguApps.Appwrite.Server/Clients/UsersClient.cs
@@ -446,9 +446,22 @@ public class UsersClient : IUsersClient
         }
     }
 
-    [ExcludeFromCodeCoverage]
     /// <inheritdoc/>
-    public Task<AppwriteResult<User>> UpdatePassword(UpdatePasswordRequest request) => throw new NotImplementedException();
+    public async Task<AppwriteResult<User>> UpdatePassword(UpdatePasswordRequest request)
+    {
+        try
+        {
+            request.Validate(true);
+
+            var result = await _usersApi.UpdatePassword(request.UserId, request);
+
+            return result.GetApiResponse();
+        }
+        catch (Exception e)
+        {
+            return e.GetExceptionResponse<User>();
+        }
+    }
 
     [ExcludeFromCodeCoverage]
     /// <inheritdoc/>
