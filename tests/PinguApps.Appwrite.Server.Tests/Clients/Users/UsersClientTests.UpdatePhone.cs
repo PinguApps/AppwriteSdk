5d3bdd4d6baed905153133d2b97808fc71c7ae88
Matthew Parker <pingu@pinguapps.com>
2024-10-09 22:24:36 +0100

    added tests for update phone

---
+++ tests/PinguApps.Appwrite.Server.Tests/Clients/Users/UsersClientTests.UpdatePhone.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Requests.Users;
+using PinguApps.Appwrite.Shared.Tests;
+using PinguApps.Appwrite.Shared.Utils;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Server.Tests.Clients.Users;
+public partial class UsersClientTests
+{
+    [Fact]
+    public async Task UpdatePhone_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        var request = new UpdatePhoneRequest
+        {
+            UserId = IdUtils.GenerateUniqueId(),
+            PhoneNumber = "+1234567890"
+        };
+
+        _mockHttp.Expect(HttpMethod.Patch, $"{Constants.Endpoint}/users/{request.UserId}/phone")
+            .ExpectedHeaders()
+            .Respond(Constants.AppJson, Constants.UserResponse);
+
+        // Act
+        var result = await _appwriteClient.Users.UpdatePhone(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task UpdatePhone_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new UpdatePhoneRequest
+        {
+            UserId = IdUtils.GenerateUniqueId(),
+            PhoneNumber = "+1234567890"
+        };
+
+        _mockHttp.Expect(HttpMethod.Patch, $"{Constants.Endpoint}/users/{request.UserId}/phone")
+            .ExpectedHeaders()
+            .Respond(HttpStatusCode.BadRequest, Constants.AppJson, Constants.AppwriteError);
+
+        // Act
+        var result = await _appwriteClient.Users.UpdatePhone(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task UpdatePhone_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new UpdatePhoneRequest
+        {
+            UserId = IdUtils.GenerateUniqueId(),
+            PhoneNumber = "+1234567890"
+        };
+
+        _mockHttp.Expect(HttpMethod.Patch, $"{Constants.Endpoint}/users/{request.UserId}/phone")
+            .ExpectedHeaders()
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteClient.Users.UpdatePhone(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
