97e6b649586fae02feb9162c8c71138840143bab
Matthew Parker <pingu@pinguapps.com>
2024-10-13 16:27:44 +0100

    added client tests

---
+++ tests/PinguApps.Appwrite.Client.Tests/Clients/Account/AccountClientTests.CreatePushTarget.cs
@@ -0,0 +1,108 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Client.Clients;
+using PinguApps.Appwrite.Shared.Requests.Account;
+using PinguApps.Appwrite.Shared.Tests;
+using PinguApps.Appwrite.Shared.Utils;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Client.Tests.Clients.Account;
+public partial class AccountClientTests
+{
+    [Fact]
+    public async Task CreatePushTarget_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        var request = new CreatePushTargetRequest
+        {
+            TargetId = IdUtils.GenerateUniqueId(),
+            Identifier = "token",
+            ProviderId = "provider123"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{TestConstants.Endpoint}/account/targets/push")
+            .ExpectedHeaders()
+            .WithJsonContent(request, _jsonSerializerOptions)
+            .Respond(TestConstants.AppJson, TestConstants.TargetResponse);
+
+        _appwriteClient.SetSession(TestConstants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.CreatePushTarget(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task CreatePushTarget_ShouldReturnError_WhenSessionIsNull()
+    {
+        // Arrange
+        var request = new CreatePushTargetRequest
+        {
+            TargetId = IdUtils.GenerateUniqueId(),
+            Identifier = "token",
+            ProviderId = "provider123"
+        };
+
+        // Act
+        var result = await _appwriteClient.Account.CreatePushTarget(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsInternalError);
+        Assert.Equal(ISessionAware.SessionExceptionMessage, result.Result.AsT2.Message);
+    }
+
+    [Fact]
+    public async Task CreatePushTarget_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new CreatePushTargetRequest
+        {
+            TargetId = IdUtils.GenerateUniqueId(),
+            Identifier = "token",
+            ProviderId = "provider123"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{TestConstants.Endpoint}/account/targets/push")
+            .ExpectedHeaders()
+            .WithJsonContent(request, _jsonSerializerOptions)
+            .Respond(HttpStatusCode.BadRequest, TestConstants.AppJson, TestConstants.AppwriteError);
+
+        _appwriteClient.SetSession(TestConstants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.CreatePushTarget(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task CreatePushTarget_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new CreatePushTargetRequest
+        {
+            TargetId = IdUtils.GenerateUniqueId(),
+            Identifier = "token",
+            ProviderId = "provider123"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{TestConstants.Endpoint}/account/targets/push")
+            .ExpectedHeaders()
+            .WithJsonContent(request, _jsonSerializerOptions)
+            .Throw(new HttpRequestException("An error occurred"));
+
+        _appwriteClient.SetSession(TestConstants.Session);
+
+        // Act
+        var result = await _appwriteClient.Account.CreatePushTarget(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
