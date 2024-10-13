71a26d2c619607ac7e908ba4bfc2814e21de343b
Matthew Parker <pingu@pinguapps.com>
2024-10-13 03:01:32 +0100

    added exclude to base query class

--- src/PinguApps.Appwrite.Shared/Requests/QueryBaseRequest.cs
+++ src/PinguApps.Appwrite.Shared/Requests/QueryBaseRequest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using FluentValidation;
+using PinguApps.Appwrite.Shared.Attributes;
 using PinguApps.Appwrite.Shared.Utils;
 
 namespace PinguApps.Appwrite.Shared.Requests;
@@ -18,5 +19,6 @@ public abstract class QueryBaseRequest<TRequest, TValidator> : BaseRequest<TRequ
     /// Array of query strings generated using the Query class provided by the SDK. <see href="https://appwrite.io/docs/queries">Learn more about queries</see>. Maximum of 100 queries are allowed, each 4096 characters long.
     /// </summary>
     [JsonPropertyName("queries")]
+    [SdkExclude]
     public virtual List<Query>? Queries { get; set; } = null;
 }
