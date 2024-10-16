2d33e74edac5a040018295471a11d2e83703959e
Matthew Parker <pingu@pinguapps.com>
2024-10-17 00:59:58 +0100

    Added create team request plus tests

---
+++ src/PinguApps.Appwrite.Shared/Requests/Teams/CreateTeamRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using PinguApps.Appwrite.Shared.Requests.Teams.Validators;
+using PinguApps.Appwrite.Shared.Utils;
+
+namespace PinguApps.Appwrite.Shared.Requests.Teams;
+
+/// <summary>
+/// The request for creating a team
+/// </summary>
+public class CreateTeamRequest : BaseRequest<CreateTeamRequest, CreateTeamRequestValidator>
+{
+    /// <summary>
+    /// Team ID. Choose a custom ID or generate a random ID with ID.unique(). Valid chars are a-z, A-Z, 0-9, period, hyphen, and underscore. Can't start with a special char. Max length is 36 chars.
+    /// </summary>
+    public string TeamId { get; set; } = IdUtils.GenerateUniqueId();
+
+    /// <summary>
+    /// Team name. Max length: 128 chars.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Array of strings. Use this param to set the roles in the team for the user who created it. The default role is <c>owner</c>. A role can be any string. Learn more about <see href="https://appwrite.io/docs/permissions">roles and permissions</see>. Maximum of 100 roles are allowed, each 32 characters long.
+    /// </summary>
+    public List<string>? Roles { get; set; }
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Teams/Validators/CreateTeamRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Teams.Validators;
+public class CreateTeamRequestValidator : AbstractValidator<CreateTeamRequest>
+{
+    public CreateTeamRequestValidator()
+    {
+        RuleFor(x => x.TeamId)
+            .NotEmpty()
+            .WithMessage("TeamId is required.")
+            .Matches("^[a-zA-Z0-9][a-zA-Z0-9._-]{0,35}$")
+            .WithMessage("TeamId can only contain a-z, A-Z, 0-9, period, hyphen, and underscore, and can't start with a special char. Max length is 36 chars.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Team name is required.")
+            .MaximumLength(128)
+            .WithMessage("Team name must be less than or equal to 128 characters.");
+
+        RuleFor(x => x.Roles)
+            .Must(x => x == null || x.Count <= 100)
+            .WithMessage("A maximum of 100 roles are allowed.")
+            .ForEach(x => x.NotEmpty()
+                .WithMessage("Each role must have a non empty string value")
+                .MaximumLength(32)
+                .WithMessage("Each role must be less than or equal to 32 characters."));
+    }
+}
