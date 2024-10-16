fc83c71e06600cec6ffaed38e9cee2cd1a746bc3
Matthew Parker <pingu@pinguapps.com>
2024-10-17 00:02:21 +0100

    added teamslist response object

---
+++ src/PinguApps.Appwrite.Shared/Responses/TeamsList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PinguApps.Appwrite.Shared.Responses;
+
+/// <summary>
+/// An Appwrite TeamsList object
+/// </summary>
+/// <param name="Total">Total number of teams documents that matched your query</param>
+/// <param name="Teams">List of teams. Can be one of: <see cref="Team"/></param>
+public record TeamsList(
+    [property: JsonPropertyName("total")] int Total,
+    [property: JsonPropertyName("teams")] IReadOnlyList<Team> Teams
+);
