59b238ba898435046f9c0f365b01579fd181f482
Matthew Parker <pingu@pinguapps.com>
2024-10-17 01:03:16 +0100

    added get team request plus tests

---
+++ src/PinguApps.Appwrite.Shared/Requests/Teams/GetTeamRequest.cs
@@ -0,0 +1,10 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Teams.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Teams;
+
+/// <summary>
+/// The request for getting a team by its Id
+/// </summary>
+public class GetTeamRequest : TeamIdBaseRequest<GetTeamRequest, GetTeamRequestValidator>
+{
+}
---
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/Teams/GetTeamRequestTests.cs
@@ -0,0 +1,8 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Teams;
+using PinguApps.Appwrite.Shared.Requests.Teams.Validators;
+
+namespace PinguApps.Appwrite.Shared.Tests.Requests.Teams;
+public class GetTeamRequestTests : TeamIdBaseRequestTests<GetTeamRequest, GetTeamRequestValidator>
+{
+    protected override GetTeamRequest CreateValidRequest => new();
+}
