d050d29ded4d4edb776e3e724dcb4d9f162b1025
Matthew Parker <pingu@pinguapps.com>
2024-10-17 19:12:42 +0100

    added delete team membership request plus tests

---
+++ src/PinguApps.Appwrite.Shared/Requests/Teams/DeleteTeamMembershipRequest.cs
@@ -0,0 +1,11 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Teams;
+using PinguApps.Appwrite.Shared.Requests.Teams.Validators;
+
+namespace PinguApps.Appwrite.Shared.Reqests.Teams;
+
+/// <summary>
+/// The reuqest for removing a user from a team
+/// </summary>
+public class DeleteTeamMembershipRequest : TeamMembershipIdBaseRequest<DeleteTeamMembershipRequest, DeleteTeamMembershipRequestValidator>
+{
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Teams/Validators/DeleteTeamMembershipRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using PinguApps.Appwrite.Shared.Reqests.Teams;
+
+namespace PinguApps.Appwrite.Shared.Requests.Teams.Validators;
+public class DeleteTeamMembershipRequestValidator : AbstractValidator<DeleteTeamMembershipRequest>
+{
+    public DeleteTeamMembershipRequestValidator()
+    {
+        Include(new TeamMembershipIdBaseRequestValidator<DeleteTeamMembershipRequest, DeleteTeamMembershipRequestValidator>());
+    }
+}
