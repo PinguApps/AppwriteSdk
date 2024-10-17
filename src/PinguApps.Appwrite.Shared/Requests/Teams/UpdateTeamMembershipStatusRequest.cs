7a5dca739ed5baf13215092d2d3a11a4af2f4f42
Matthew Parker <pingu@pinguapps.com>
2024-10-17 19:22:26 +0100

    added update team membership status request plus tests

---
+++ src/PinguApps.Appwrite.Shared/Requests/Teams/UpdateTeamMembershipStatusRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Teams.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Teams;
+
+/// <summary>
+/// The request for accepting a users invitation to join a team
+/// </summary>
+public class UpdateTeamMembershipStatusRequest : TeamMembershipIdBaseRequest<UpdateTeamMembershipStatusRequest, UpdateTeamMembershipStatusRequestValidator>
+{
+    /// <summary>
+    /// User ID
+    /// </summary>
+    [JsonPropertyName("userId")]
+    public string UserId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Secret Key
+    /// </summary>
+    [JsonPropertyName("secret")]
+    public string Secret { get; set; } = string.Empty;
+}
