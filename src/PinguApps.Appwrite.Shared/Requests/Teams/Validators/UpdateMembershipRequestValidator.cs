a01e86672b1373749f2386486920218a0172d22b
Matthew Parker <pingu@pinguapps.com>
2024-10-17 19:08:41 +0100

    added request for update membership plus tests

---
+++ src/PinguApps.Appwrite.Shared/Requests/Teams/Validators/UpdateMembershipRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Teams.Validators;
+public class UpdateMembershipRequestValidator : AbstractValidator<UpdateMembershipRequest>
+{
+    public UpdateMembershipRequestValidator()
+    {
+        Include(new TeamMembershipIdBaseRequestValidator<UpdateMembershipRequest, UpdateMembershipRequestValidator>());
+
+        RuleFor(x => x.Roles)
+            .NotNull()
+            .WithMessage("Roles cannot be null.")
+            .Must(x => x.Count <= 100)
+            .WithMessage("A maximum of 100 roles are allowed.")
+            .ForEach(x => x
+                .MaximumLength(32)
+                .WithMessage("Each role can be a maximum of 32 characters long."));
+    }
+}
