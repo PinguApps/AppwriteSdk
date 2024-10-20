4e4d5a4434a7e7190947da44ba7448db90eca59d
Matthew Parker <pingu@pinguapps.com>
2024-10-20 23:45:40 +0100

    Added databases list response

---
+++ src/PinguApps.Appwrite.Shared/Responses/DatabasesList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PinguApps.Appwrite.Shared.Responses;
+
+/// <summary>
+/// An Appwrite Databases List object
+/// </summary>
+/// <param name="Total">Total number of databases documents that matched your query</param>
+/// <param name="Databases">List of databases. Can be one of: <see cref="Database"/></param>
+public record DatabasesList(
+    [property: JsonPropertyName("total")] int Total,
+    [property: JsonPropertyName("databases")] IReadOnlyList<Database> Databases
+);
