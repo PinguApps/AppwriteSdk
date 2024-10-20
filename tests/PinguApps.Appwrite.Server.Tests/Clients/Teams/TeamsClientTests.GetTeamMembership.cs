c6e65e440f47096e61e86efc792d79ced9414025
Matthew Parker <pingu@pinguapps.com>
2024-10-20 16:10:02 +0100

    added tests for get team membership

---
+++ tests/PinguApps.Appwrite.Server.Tests/Clients/Teams/TeamsClientTests.GetTeamMembership.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Requests.Teams;
+using PinguApps.Appwrite.Shared.Tests;
+using PinguApps.Appwrite.Shared.Utils;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Server.Tests.Clients.Teams;
+public partial class TeamsClientTests
+{
+    [Fact]
+    public async Task GetTeamMembership_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        var request = new GetTeamMembershipRequest
+        {
+            TeamId = IdUtils.GenerateUniqueId(),
+            MembershipId = IdUtils.GenerateUniqueId()
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{TestConstants.Endpoint}/teams/{request.TeamId}/memberships/{request.MembershipId}")
+            .ExpectedHeaders()
+            .Respond(TestConstants.AppJson, TestConstants.MembershipResponse);
+
+        // Act
+        var result = await _appwriteClient.Teams.GetTeamMembership(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task GetTeamMembership_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new GetTeamMembershipRequest
+        {
+            TeamId = IdUtils.GenerateUniqueId(),
+            MembershipId = IdUtils.GenerateUniqueId()
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{TestConstants.Endpoint}/teams/{request.TeamId}/memberships/{request.MembershipId}")
+            .ExpectedHeaders()
+            .Respond(HttpStatusCode.BadRequest, TestConstants.AppJson, TestConstants.AppwriteError);
+
+        // Act
+        var result = await _appwriteClient.Teams.GetTeamMembership(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task GetTeamMembership_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new GetTeamMembershipRequest
+        {
+            TeamId = IdUtils.GenerateUniqueId(),
+            MembershipId = IdUtils.GenerateUniqueId()
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{TestConstants.Endpoint}/teams/{request.TeamId}/memberships/{request.MembershipId}")
+            .ExpectedHeaders()
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteClient.Teams.GetTeamMembership(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
