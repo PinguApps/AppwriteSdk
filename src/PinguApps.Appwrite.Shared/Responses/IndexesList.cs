088aec57479fa04852c227fae393705ec3c5cf85
Matthew Parker <pingu@pinguapps.com>
2024-10-24 18:58:55 +0100

    added indexes list response

---
+++ src/PinguApps.Appwrite.Shared/Responses/IndexesList.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PinguApps.Appwrite.Shared.Responses;
+public record IndexesList(
+    [property: JsonPropertyName("total")] int Total,
+    [property: JsonPropertyName("indexes")] IReadOnlyList<Index> Indexes
+);
