18803f5381ae123e5264239936a680516b170406
Matthew Parker <pingu@pinguapps.com>
2024-10-25 00:32:13 +0100

    Added DocumentsList - still needs converter

---
+++ src/PinguApps.Appwrite.Shared/Responses/DocumentsList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PinguApps.Appwrite.Shared.Responses;
+
+/// <summary>
+/// An Appwrite DocumentsList attribute
+/// </summary>
+/// <param name="Total">Total number of documents documents that matched your query</param>
+/// <param name="Documents">List of documents</param>
+public record DocumentsList(
+    [property: JsonPropertyName("total")] int Total,
+    [property: JsonPropertyName("documents"), JsonConverter(typeof(DocumentListConverter))] IReadOnlyList<Document> Documents
+);
