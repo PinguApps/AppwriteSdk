05e67c92c43b2aa5904e2a50e77192b53cff792a
Matthew Parker <pingu@pinguapps.com>
2024-10-26 15:37:06 +0100

    Added List database request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/ListDatabasesRequest.cs
@@ -0,0 +1,10 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request for listing databases
+/// </summary>
+public class ListDatabasesRequest : QuerySearchBaseRequest<ListDatabasesRequest, ListDatabasesRequestValidator>
+{
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/ListDatabasesRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using PinguApps.Appwrite.Shared.Requests.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class ListDatabasesRequestValidator : AbstractValidator<ListDatabasesRequest>
+{
+    public ListDatabasesRequestValidator()
+    {
+        Include(new QuerySearchBaseRequestValidator<ListDatabasesRequest, ListDatabasesRequestValidator>());
+    }
+}
