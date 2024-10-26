3af8d23765685da0122ce7a35f42014d5a457ac8
Matthew Parker <pingu@pinguapps.com>
2024-10-26 23:00:22 +0100

    Added create collection request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/CreateCollectionRequest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Converters;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+using PinguApps.Appwrite.Shared.Utils;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request for creating a collection
+/// </summary>
+public class CreateCollectionRequest : DatabaseIdBaseRequest<CreateCollectionRequest, CreateCollectionRequestValidator>
+{
+    /// <summary>
+    /// Unique Id. Choose a custom ID or generate a random ID with ID.unique(). Valid chars are a-z, A-Z, 0-9, period, hyphen, and underscore. Can't start with a special char. Max length is 36 chars
+    /// </summary>
+    [JsonPropertyName("collectionId")]
+    public string CollectionId { get; set; } = IdUtils.GenerateUniqueId();
+
+    /// <summary>
+    /// Collection name. Max length: 128 chars
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// An array of permissions strings. By default, no user is granted with any permissions. <see href="https://appwrite.io/docs/permissions">Learn more about permissions</see>.
+    /// </summary>
+    [JsonPropertyName("permissions")]
+    [JsonConverter(typeof(PermissionListConverter))]
+    public List<Permission> Permissions { get; set; } = [];
+
+    /// <summary>
+    /// Enables configuring permissions for individual documents. A user needs one of document or collection level permissions to access a document. <see href="https://appwrite.io/docs/permissions">Learn more about permissions</see>.
+    /// </summary>
+    [JsonPropertyName("documentSecurity")]
+    public bool DocumentSecurity { get; set; }
+
+    /// <summary>
+    /// Is collection enabled? When set to 'disabled', users cannot access the collection but Server SDKs with and API key can still read and write to the collection. No data is lost when this is toggled.
+    /// </summary>
+    [JsonPropertyName("enabled")]
+    public bool Enabled { get; set; }
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/CreateCollectionRequestValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class CreateCollectionRequestValidator : AbstractValidator<CreateCollectionRequest>
+{
+    public CreateCollectionRequestValidator()
+    {
+        Include(new DatabaseIdBaseRequestValidator<CreateCollectionRequest, CreateCollectionRequestValidator>());
+
+        RuleFor(x => x.CollectionId)
+            .NotEmpty()
+            .WithMessage("CollectionId is required.")
+            .Matches("^[a-zA-Z0-9][a-zA-Z0-9._-]{0,35}$")
+            .WithMessage("CollectionId can only contain a-z, A-Z, 0-9, period, hyphen, and underscore, and can't start with a special character. Max length is 36 characters.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MaximumLength(128)
+            .WithMessage("Name cannot exceed 128 characters.");
+
+        RuleFor(x => x.Permissions)
+            .NotNull()
+            .WithMessage("Permissions cannot be null.");
+
+        RuleFor(x => x.DocumentSecurity)
+            .NotNull()
+            .WithMessage("DocumentSecurity is required.");
+
+        RuleFor(x => x.Enabled)
+            .NotNull()
+            .WithMessage("Enabled is required.");
+    }
+}
