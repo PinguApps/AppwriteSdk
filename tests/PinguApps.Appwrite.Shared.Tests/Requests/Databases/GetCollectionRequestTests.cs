257cd13e7b51eb0482a50a98c7f4aae3cef18419
Matthew Parker <pingu@pinguapps.com>
2024-10-26 23:03:51 +0100

    added tests for get collection request

---
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/Databases/GetCollectionRequestTests.cs
@@ -0,0 +1,8 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Tests.Requests.Databases;
+public class GetCollectionRequestTests : DatabaseCollectionIdBaseRequestTests<GetCollectionRequest, GetCollectionRequestValidator>
+{
+    protected override GetCollectionRequest CreateValidDatabaseCollectionIdRequest => new();
+}
