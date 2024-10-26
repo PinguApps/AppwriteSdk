d80a192c185ece83c4226be0efe617f0c513a86f
Matthew Parker <pingu@pinguapps.com>
2024-10-26 01:17:39 +0100

    added database Id base request, validator, and tests

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/DatabaseIdBaseRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+using FluentValidation;
+using PinguApps.Appwrite.Shared.Attributes;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The base request but also containing DatabaseId
+/// </summary>
+/// <typeparam name="TRequest">The request type</typeparam>
+/// <typeparam name="TValidator">The request validator type</typeparam>
+public abstract class DatabaseIdBaseRequest<TRequest, TValidator> : BaseRequest<TRequest, TValidator>
+    where TRequest : class
+    where TValidator : IValidator<TRequest>, new()
+{
+    /// <summary>
+    /// Database ID
+    /// </summary>
+    [JsonPropertyName("databaseId")]
+    [SdkExclude]
+    public string DatabaseId { get; set; } = string.Empty;
+}
---
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/Databases/DatabaseIdBaseRequestTests.cs
@@ -0,0 +1,89 @@
+﻿using FluentValidation;
+using PinguApps.Appwrite.Shared.Requests.Databases;
+
+namespace PinguApps.Appwrite.Shared.Tests.Requests.Databases;
+public abstract class DatabaseIdBaseRequestTests<TRequest, TValidator>
+        where TRequest : DatabaseIdBaseRequest<TRequest, TValidator>
+        where TValidator : AbstractValidator<TRequest>, new()
+{
+    protected abstract TRequest CreateValidRequest { get; }
+
+    [Fact]
+    public void TeamIdBase_Constructor_InitializesWithExpectedValues()
+    {
+        // Arrange & Act
+        var request = CreateValidRequest;
+
+        // Assert
+        Assert.Equal(string.Empty, request.DatabaseId);
+    }
+
+    [Fact]
+    public void TeamIdBase_Properties_CanBeSet()
+    {
+        // Arrange
+        var teamIdValue = "validId";
+        var request = CreateValidRequest;
+
+        // Act
+        request.DatabaseId = teamIdValue;
+
+        // Assert
+        Assert.Equal(teamIdValue, request.DatabaseId);
+    }
+
+    [Fact]
+    public void TeamIdBase_IsValid_WithValidTeamId_ReturnsTrue()
+    {
+        // Arrange
+        var request = CreateValidRequest;
+        request.DatabaseId = "valid_Team-Id.";
+
+        // Act
+        var isValid = request.IsValid();
+
+        // Assert
+        Assert.True(isValid);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void TeamIdBase_IsValid_WithInvalidData_ReturnsFalse(string? id)
+    {
+        // Arrange
+        var request = CreateValidRequest;
+        request.DatabaseId = id!;
+
+        // Act
+        var isValid = request.IsValid();
+
+        // Assert
+        Assert.False(isValid);
+    }
+
+    [Fact]
+    public void TeamIdBase_Validate_WithThrowOnFailuresTrue_ThrowsValidationExceptionOnFailure()
+    {
+        // Arrange
+        var request = CreateValidRequest;
+        request.DatabaseId = string.Empty; // Invalid Id
+
+        // Assert
+        Assert.Throws<ValidationException>(() => request.Validate(true));
+    }
+
+    [Fact]
+    public void TeamIdBase_Validate_WithThrowOnFailuresFalse_ReturnsInvalidResultOnFailure()
+    {
+        // Arrange
+        var request = CreateValidRequest;
+        request.DatabaseId = string.Empty; // Invalid Id
+
+        // Act
+        var result = request.Validate(false);
+
+        // Assert
+        Assert.False(result.IsValid);
+    }
+}
