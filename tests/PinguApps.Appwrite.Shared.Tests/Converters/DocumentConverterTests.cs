4731a5f588a1cd677aff95b5adefa230f3f90e9b
Matthew Parker <pingu@pinguapps.com>
2024-10-26 23:07:53 +0100

    Added missing test to document converter tests

--- tests/PinguApps.Appwrite.Shared.Tests/Converters/DocumentConverterTests.cs
+++ tests/PinguApps.Appwrite.Shared.Tests/Converters/DocumentConverterTests.cs
@@ -229,6 +229,21 @@ public class DocumentConverterTests
     }
 
     [Fact]
+    public void Read_MissingDatabaseId_ThrowsJsonException()
+    {
+        var json = @"
+        {
+            ""$id"": ""1"",
+            ""$collectionId"": ""col1"",
+            ""$createdAt"": ""2020-10-15T06:38:00.000+00:00"",
+            ""$updatedAt"": ""2020-10-15T06:38:00.000+00:00"",
+            ""$permissions"": [""read(\""any\"")""]
+        }";
+
+        Assert.Throws<JsonException>(() => JsonSerializer.Deserialize<Document>(json, _options));
+    }
+
+    [Fact]
     public void Read_MissingCreatedAt_ThrowsJsonException()
     {
         var json = @"
