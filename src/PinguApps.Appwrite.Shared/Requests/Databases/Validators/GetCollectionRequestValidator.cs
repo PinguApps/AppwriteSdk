4c22a7f27905f745262962386a490f215619968e
Matthew Parker <pingu@pinguapps.com>
2024-10-26 23:02:47 +0100

    added get collection request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/GetCollectionRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class GetCollectionRequestValidator : AbstractValidator<GetCollectionRequest>
+{
+    public GetCollectionRequestValidator()
+    {
+        Include(new DatabaseCollectionIdBaseRequestValidator<GetCollectionRequest, GetCollectionRequestValidator>());
+    }
+}
