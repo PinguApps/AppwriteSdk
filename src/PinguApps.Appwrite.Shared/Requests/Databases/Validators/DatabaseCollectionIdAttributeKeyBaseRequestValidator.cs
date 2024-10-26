2b4a8b23281dd20479124479cfda55845f0d816f
Matthew Parker <pingu@pinguapps.com>
2024-10-26 15:03:57 +0100

    added validator for database collection id attribute key base request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/DatabaseCollectionIdAttributeKeyBaseRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class DatabaseCollectionIdAttributeKeyBaseRequestValidator<TRequest, TValidator> : AbstractValidator<DatabaseCollectionIdAttributeKeyBaseRequest<TRequest, TValidator>>
+    where TRequest : class
+    where TValidator : IValidator<TRequest>, new()
+{
+    public DatabaseCollectionIdAttributeKeyBaseRequestValidator()
+    {
+        Include(new DatabaseCollectionIdBaseRequestValidator<TRequest, TValidator>());
+
+        RuleFor(x => x.Key)
+            .NotEmpty()
+            .WithMessage("Key is required.");
+    }
+}
