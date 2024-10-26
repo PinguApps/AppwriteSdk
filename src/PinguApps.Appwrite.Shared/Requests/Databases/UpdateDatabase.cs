a9d6384c7acb3a5ab9d35faa5d6e0945fb130ef9
Matthew Parker <pingu@pinguapps.com>
2024-10-26 16:11:42 +0100

    added update database request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/UpdateDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+public class UpdateDatabase : DatabaseIdBaseRequest<UpdateDatabase, UpdateDatabaseValidator>
+{
+    /// <summary>
+    /// Database name. Max length: 128 chars
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Is database enabled? When set to 'disabled', users cannot access the database but Server SDKs with an API key can still read and write to the database. No data is lost when this is toggled
+    /// </summary>
+    [JsonPropertyName("enabled")]
+    public bool Enabled { get; set; }
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/UpdateDatabaseValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class UpdateDatabaseValidator : AbstractValidator<UpdateDatabase>
+{
+    public UpdateDatabaseValidator()
+    {
+        Include(new DatabaseIdBaseRequestValidator<UpdateDatabase, UpdateDatabaseValidator>());
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MaximumLength(128)
+            .WithMessage("Name cannot exceed 128 characters.");
+
+        RuleFor(x => x.Enabled)
+            .NotNull()
+            .WithMessage("Enabled is required.");
+    }
+}
