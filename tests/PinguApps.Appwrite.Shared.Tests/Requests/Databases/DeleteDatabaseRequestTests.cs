6968e117013649b7a00d20c3a955e345b2d3e245
Matthew Parker <pingu@pinguapps.com>
2024-10-26 16:16:10 +0100

    added tests for delete database request

---
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/Databases/DeleteDatabaseRequestTests.cs
@@ -0,0 +1,8 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Tests.Requests.Databases;
+public class DeleteDatabaseRequestTests : DatabaseIdBaseRequestTests<DeleteDatabaseRequest, DeleteDatabaseRequestValidator>
+{
+    protected override DeleteDatabaseRequest CreateValidRequest => new();
+}
