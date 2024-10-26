ce1fe4d334533727df28ae8c085e12dcb40de1f7
Matthew Parker <pingu@pinguapps.com>
2024-10-27 00:37:59 +0100

    added delete collection request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/DeleteCollectionRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class DeleteCollectionRequestValidator : AbstractValidator<DeleteCollectionRequest>
+{
+    public DeleteCollectionRequestValidator()
+    {
+        Include(new DatabaseCollectionIdBaseRequestValidator<DeleteCollectionRequest, DeleteCollectionRequestValidator>());
+    }
+}
