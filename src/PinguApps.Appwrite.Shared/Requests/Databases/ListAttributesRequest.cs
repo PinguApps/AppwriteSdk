619a8ea57f0a7e71d77266e565ebda0c51f6a8fd
Matthew Parker <pingu@pinguapps.com>
2024-10-27 00:44:06 +0100

    added request to list attributes

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/ListAttributesRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Attributes;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request to list attributes of a collection
+/// </summary>
+public class ListAttributesRequest : QueryBaseRequest<ListAttributesRequest, ListAttributesRequestValidator>
+{
+    /// <summary>
+    /// Database ID
+    /// </summary>
+    [JsonPropertyName("databaseId")]
+    [SdkExclude]
+    public string DatabaseId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Collection ID
+    /// </summary>
+    [JsonPropertyName("databaseId")]
+    [SdkExclude]
+    public string CollectionId { get; set; } = string.Empty;
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/ListAttributesRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using PinguApps.Appwrite.Shared.Requests.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class ListAttributesRequestValidator : AbstractValidator<ListAttributesRequest>
+{
+    public ListAttributesRequestValidator()
+    {
+        Include(new QueryBaseRequestValidator<ListAttributesRequest, ListAttributesRequestValidator>());
+
+        RuleFor(x => x.DatabaseId)
+            .NotEmpty()
+            .WithMessage("DatabaseId is required.");
+
+        RuleFor(x => x.CollectionId)
+            .NotEmpty()
+            .WithMessage("CollectionId is required.");
+    }
+}
