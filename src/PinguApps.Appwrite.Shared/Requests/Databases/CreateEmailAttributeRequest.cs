522474fd374e1a2786f3886dd139931270b08d6a
Matthew Parker <pingu@pinguapps.com>
2024-10-27 13:53:26 +0000

    added create email attribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/CreateEmailAttributeRequest.cs
@@ -0,0 +1,10 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request to create an email attribute
+/// </summary>
+public class CreateEmailAttributeRequest : CreateStringAttributeBaseRequest<CreateEmailAttributeRequest, CreateEmailAttributeRequestValidator>
+{
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/CreateEmailAttributeRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class CreateEmailAttributeRequestValidator : AbstractValidator<CreateEmailAttributeRequest>
+{
+    public CreateEmailAttributeRequestValidator()
+    {
+        Include(new CreateStringAttributeBaseRequestValidator<CreateEmailAttributeRequest, CreateEmailAttributeRequestValidator>());
+
+        RuleFor(x => x.Default)
+            .EmailAddress()
+            .WithMessage("Default should be formatted as an email address");
+    }
+}
