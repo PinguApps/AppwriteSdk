0eb2023e55f07af0ce436572b3a43b105fe9bed6
Matthew Parker <pingu@pinguapps.com>
2024-10-27 01:46:01 +0000

    added update datetime atttribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/UpdateDatetimeAttributeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Converters;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+public class UpdateDatetimeAttributeRequest : UpdateAttributeBaseRequest<UpdateDatetimeAttributeRequest, UpdateDatetimeAttributeRequestValidator>
+{
+    /// <summary>
+    /// Default value for attribute when not provided. Cannot be set when attribute is required
+    /// </summary>
+    [JsonPropertyName("default")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    [JsonConverter(typeof(AlwaysWriteNullableDateTimeConverter))]
+    public DateTime? Default { get; set; }
+}
