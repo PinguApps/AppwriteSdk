52b8a3cce097ae6ed6fa98f2b11a47c37a56c901
Matthew Parker <pingu@pinguapps.com>
2024-10-27 14:06:25 +0000

    added update email attribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/UpdateEmailAttributeRequest.cs
@@ -0,0 +1,10 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request to update an email attribute
+/// </summary>
+public class UpdateEmailAttributeRequest : UpdateStringAttributeBaseRequest<UpdateEmailAttributeRequest, UpdateEmailAttributeRequestValidator>
+{
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/UpdateEmailAttributeRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class UpdateEmailAttributeRequestValidator : AbstractValidator<UpdateEmailAttributeRequest>
+{
+    public UpdateEmailAttributeRequestValidator()
+    {
+        Include(new UpdateStringAttributeBaseRequestValidator<UpdateEmailAttributeRequest, UpdateEmailAttributeRequestValidator>());
+
+        RuleFor(x => x.Default)
+            .EmailAddress()
+            .WithMessage("Default should be formatted as an email address");
+    }
+}
