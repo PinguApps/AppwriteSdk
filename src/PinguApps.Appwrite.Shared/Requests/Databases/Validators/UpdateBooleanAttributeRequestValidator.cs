76431fdf15480aee9de08531cd57dc6275d18e85
Matthew Parker <pingu@pinguapps.com>
2024-10-27 01:56:49 +0100

    added update boolean attribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/UpdateBooleanAttributeRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class UpdateBooleanAttributeRequestValidator : AbstractValidator<UpdateBooleanAttributeRequest>
+{
+    public UpdateBooleanAttributeRequestValidator()
+    {
+        Include(new UpdateAttributeBaseRequestValidator<UpdateBooleanAttributeRequest, UpdateBooleanAttributeRequestValidator>());
+
+        RuleFor(x => x.Default)
+            .Must((request, defaultValue) => !request.Required || defaultValue == null)
+            .WithMessage("Default value cannot be set when attribute is required.");
+    }
+}
