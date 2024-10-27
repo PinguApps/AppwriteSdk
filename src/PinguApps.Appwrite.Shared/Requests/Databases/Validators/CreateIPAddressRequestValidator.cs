7a1aab27d1c353f2a114edf50d6344a49ace04b7
Matthew Parker <pingu@pinguapps.com>
2024-10-27 18:26:38 +0000

    Added create IP Address attreibute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/CreateIPAddressRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class CreateIPAddressRequestValidator : AbstractValidator<CreateIPAddressRequest>
+{
+    public CreateIPAddressRequestValidator()
+    {
+        Include(new CreateStringAttributeBaseRequestValidator<CreateIPAddressRequest, CreateIPAddressRequestValidator>());
+    }
+}
