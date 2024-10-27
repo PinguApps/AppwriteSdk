8cc482830056f615deafcfc19aec4f791534680c
Matthew Parker <pingu@pinguapps.com>
2024-10-27 01:35:29 +0000

    added create dateime attribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/CreateDatetimeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Converters;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+public class CreateDatetimeAttribute : CreateAttributeBaseRequest<CreateDatetimeAttribute, CreateDatetimeAttributeValidator>
+{
+    /// <summary>
+    /// Default value for the attribute in ISO 8601 format. Cannot be set when attribute is required
+    /// </summary>
+    [JsonPropertyName("default")]
+    [JsonConverter(typeof(NullableDateTimeConverter))]
+    public DateTime? Default { get; set; }
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/CreateDatetimeAttributeValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class CreateDatetimeAttributeValidator : AbstractValidator<CreateDatetimeAttribute>
+{
+    public CreateDatetimeAttributeValidator()
+    {
+        Include(new CreateAttributeBaseRequestValidator<CreateDatetimeAttribute, CreateDatetimeAttributeValidator>());
+
+        RuleFor(x => x.Default)
+            .Must((request, defaultValue) => !request.Required || defaultValue == null)
+            .WithMessage("Default value cannot be set when attribute is required.");
+    }
+}
