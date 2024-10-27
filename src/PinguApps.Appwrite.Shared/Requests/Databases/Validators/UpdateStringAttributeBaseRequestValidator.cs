512d9551913c41277a4b6250d3ad300244b7a270
Matthew Parker <pingu@pinguapps.com>
2024-10-27 13:42:29 +0000

    added base request for updating string based attributes

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/UpdateStringAttributeBaseRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class UpdateStringAttributeBaseRequestValidator<TRequest, TValidator> : AbstractValidator<UpdateStringAttributeBaseRequest<TRequest, TValidator>>
+    where TRequest : class
+    where TValidator : IValidator<TRequest>, new()
+{
+    public UpdateStringAttributeBaseRequestValidator()
+    {
+        Include(new UpdateAttributeBaseRequestValidator<TRequest, TValidator>());
+
+        RuleFor(x => x.Default)
+            .Must((request, defaultValue) => !request.Required || defaultValue is null)
+            .WithMessage("Default value cannot be set when attribute is required.");
+    }
+}
