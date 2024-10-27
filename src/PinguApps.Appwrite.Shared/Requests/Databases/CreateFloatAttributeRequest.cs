e194c94d39888251df4907cb4e2900ce7f609813
Matthew Parker <pingu@pinguapps.com>
2024-10-27 17:20:33 +0000

    Added CreateFloatAttributeRequest

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/CreateFloatAttributeRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request to create a float attribute
+/// </summary>
+public class CreateFloatAttributeRequest : CreateAttributeBaseRequest<CreateFloatAttributeRequest, CreateFloatAttributeRequestValidator>
+{
+    /// <summary>
+    /// Default value for attribute when not provided. Cannot be set when attribute is required
+    /// </summary>
+    [JsonPropertyName("default")]
+    public float? Default { get; set; }
+
+    /// <summary>
+    /// Minimum value to enforce on new documents
+    /// </summary>
+    [JsonPropertyName("min")]
+    public float? Min { get; set; }
+
+    /// <summary>
+    /// Maximum value to enforce on new documents
+    /// </summary>
+    [JsonPropertyName("max")]
+    public float? Max { get; set; }
+}
