12bc48b7f4ffbd9de15c8b30766fd89e55ea5329
Matthew Parker <pingu@pinguapps.com>
2024-10-27 01:04:47 +0100

    Added CreateAttributeBaseRequest plus tests

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/CreateAttributeBaseRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class CreateAttributeBaseRequestValidator<TRequest, TValidator> : AbstractValidator<CreateAttributeBaseRequest<TRequest, TValidator>>
+    where TRequest : class
+    where TValidator : IValidator<TRequest>, new()
+{
+    public CreateAttributeBaseRequestValidator()
+    {
+        Include(new DatabaseCollectionIdBaseRequestValidator<TRequest, TValidator>());
+
+        RuleFor(x => x.Key)
+            .NotEmpty()
+            .WithMessage("Key is required.");
+
+        RuleFor(x => x.Required)
+            .NotEmpty()
+            .WithMessage("Required is required.");
+
+        RuleFor(x => x.Array)
+            .NotEmpty()
+            .WithMessage("Array is required.");
+    }
+}
