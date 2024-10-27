079b3239ac75879ab34fa3d795c48caee4ec7e81
Matthew Parker <pingu@pinguapps.com>
2024-10-27 18:09:28 +0000

    added create integer attribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/CreateIntegerAttributeRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request to create an integer attribute
+/// </summary>
+public class CreateIntegerAttributeRequest : CreateAttributeBaseRequest<CreateIntegerAttributeRequest, CreateIntegerAttributeRequestValidator>
+{
+    /// <summary>
+    /// Default value for attribute when not provided. Cannot be set when attribute is required
+    /// </summary>
+    [JsonPropertyName("default")]
+    public int? Default { get; set; }
+
+    /// <summary>
+    /// Minimum value to enforce on new documents
+    /// </summary>
+    [JsonPropertyName("min")]
+    public int? Min { get; set; }
+
+    /// <summary>
+    /// Maximum value to enforce on new documents
+    /// </summary>
+    [JsonPropertyName("max")]
+    public int? Max { get; set; }
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/CreateIntegerAttributeRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class CreateIntegerAttributeRequestValidator : AbstractValidator<CreateIntegerAttributeRequest>
+{
+    public CreateIntegerAttributeRequestValidator()
+    {
+        Include(new CreateAttributeBaseRequestValidator<CreateIntegerAttributeRequest, CreateIntegerAttributeRequestValidator>());
+
+        RuleFor(x => x.Default)
+            .Must((request, defaultValue) => !request.Required || defaultValue is null)
+            .WithMessage("Default value cannot be set when attribute is required.")
+            .Must((request, defaultValue) => (request.Min is null || defaultValue is null) || defaultValue >= request.Min)
+            .WithMessage("Default cannot be a smaller value than Min.")
+            .Must((request, defaultValue) => (request.Max is null || defaultValue is null) || defaultValue <= request.Max)
+            .WithMessage("Default cannot be a larger value than Max.");
+
+        RuleFor(x => x.Max)
+            .Must((request, max) => (request.Min is null || max is null) || max >= request.Min)
+            .WithMessage("Max can not be a lower value than Min.");
+    }
+}
