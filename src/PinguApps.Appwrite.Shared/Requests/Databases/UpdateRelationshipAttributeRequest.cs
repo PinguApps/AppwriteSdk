345029b87110d6b72adc764f959692be287310d8
Matthew Parker <pingu@pinguapps.com>
2024-11-07 18:56:20 +0000

    added update relationship attribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/UpdateRelationshipAttributeRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Enums;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request to update a relationship attribute
+/// </summary>
+public class UpdateRelationshipAttributeRequest : DatabaseCollectionIdAttributeKeyBaseRequest<UpdateRelationshipAttributeRequest, UpdateRelationshipAttributeRequestValidator>
+{
+    /// <summary>
+    /// New attribute key
+    /// </summary>
+    [JsonPropertyName("newKey")]
+    public string? NewKey { get; set; }
+
+    /// <summary>
+    /// Constraints option
+    /// </summary>
+    [JsonPropertyName("onDelete")]
+    public OnDelete? OnDelete { get; set; }
+}
