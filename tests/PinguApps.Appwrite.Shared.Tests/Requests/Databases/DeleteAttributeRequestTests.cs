3c1b638f363f629f0a0a1b40b99b101378b65b13
Matthew Parker <pingu@pinguapps.com>
2024-11-09 09:56:39 +0000

    added tests for delete attribute request

---
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/Databases/DeleteAttributeRequestTests.cs
@@ -0,0 +1,8 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Tests.Requests.Databases;
+public class DeleteAttributeRequestTests : DatabaseCollectionIdAttributeKeyBaseRequestTests<DeleteAttributeRequest, DeleteAttributeRequestValidator>
+{
+    protected override DeleteAttributeRequest CreateValidDatabaseCollectionIdAttributeKeyRequest => new();
+}
