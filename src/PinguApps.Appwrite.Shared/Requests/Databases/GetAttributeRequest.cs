4f75d856aa94ca4200ba7f0495e0b1f511b3e689
Matthew Parker <pingu@pinguapps.com>
2024-11-09 09:50:46 +0000

    added get attribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/GetAttributeRequest.cs
@@ -0,0 +1,10 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request to get an attribute
+/// </summary>
+public class GetAttributeRequest : DatabaseCollectionIdAttributeKeyBaseRequest<GetAttributeRequest, GetAttributeRequestValidator>
+{
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/GetAttributeRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class GetAttributeRequestValidator : AbstractValidator<GetAttributeRequest>
+{
+    public GetAttributeRequestValidator()
+    {
+        Include(new DatabaseCollectionIdAttributeKeyBaseRequestValidator<GetAttributeRequest, GetAttributeRequestValidator>());
+    }
+}
