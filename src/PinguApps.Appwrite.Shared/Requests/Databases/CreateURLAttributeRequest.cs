046522f6af21651055852d206055beba18e5b218
Matthew Parker <pingu@pinguapps.com>
2024-11-09 09:27:34 +0000

    added request to create url attribute

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/CreateURLAttributeRequest.cs
@@ -0,0 +1,10 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request to create a URL attribute
+/// </summary>
+public class CreateURLAttributeRequest : CreateStringAttributeBaseRequest<CreateURLAttributeRequest, CreateURLAttributeRequestValidator>
+{
+}
---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/CreateURLAttributeRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class CreateURLAttributeRequestValidator : AbstractValidator<CreateURLAttributeRequest>
+{
+    public CreateURLAttributeRequestValidator()
+    {
+        Include(new CreateStringAttributeBaseRequestValidator<CreateURLAttributeRequest, CreateURLAttributeRequestValidator>());
+
+        RuleFor(x => x.Default)
+            .Must(x => Uri.TryCreate(x, UriKind.Absolute, out _))
+            .WithMessage("Default should be formatted as a URL");
+    }
+}
