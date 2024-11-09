7c3bfd86e06d8c7d90200a760ad96f432da83346
Matthew Parker <pingu@pinguapps.com>
2024-11-09 09:12:18 +0000

    completed tests for create string attribute request

--- tests/PinguApps.Appwrite.Shared.Tests/Requests/Databases/CreateStringAttributeRequestTests.cs
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/Databases/CreateStringAttributeRequestTests.cs
@@ -22,6 +22,68 @@ public class CreateStringAttributeRequestTests : CreateStringAttributeBaseReques
             Default = null,
             Size = 1,
             Encrypt = false
+        },
+        new()
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "validKey",
+            Default = new string('1', 10),
+            Size = 10,
+            Encrypt = true
         }
     ];
+
+    [Theory]
+    [MemberData(nameof(ValidRequests))]
+    public void IsValid_WithValidRequest_ReturnsTrue(CreateStringAttributeRequest request)
+    {
+        // Act
+        var isValid = request.IsValid();
+
+        // Assert
+        Assert.True(isValid);
+    }
+
+    public static TheoryData<CreateStringAttributeRequest> InvalidRequests =>
+    [
+        new()
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "validKey",
+            Default = null,
+            Size = 0,
+            Encrypt = false
+        },
+        new()
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "validKey",
+            Default = null,
+            Size = 1_073_741_825,
+            Encrypt = false
+        },
+        new()
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "validKey",
+            Default = new string('a', 11),
+            Size = 10,
+            Encrypt = false
+        }
+    ];
+
+    [Theory]
+    [MemberData(nameof(InvalidRequests))]
+    public void IsValid_WithInvalidRequest_ReturnsFalse(CreateStringAttributeRequest request)
+    {
+        // Act
+        var isValid = request.IsValid();
+
+        // Assert
+        Assert.False(isValid);
+    }
 }
