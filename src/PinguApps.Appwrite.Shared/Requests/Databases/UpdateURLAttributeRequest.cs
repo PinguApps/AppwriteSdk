626d90b12706621019cd793478e88423922d9761
Matthew Parker <pingu@pinguapps.com>
2024-11-09 09:46:13 +0000

    added update url attribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/UpdateURLAttributeRequest.cs
@@ -0,0 +1,10 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+
+/// <summary>
+/// The request to update a URL attribute
+/// </summary>
+public class UpdateURLAttributeRequest : UpdateStringAttributeBaseRequest<UpdateURLAttributeRequest, UpdateURLAttributeRequestValidator>
+{
+}
