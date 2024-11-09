81df6d00179e2ba79dcf4113abe7510586409073
Matthew Parker <pingu@pinguapps.com>
2024-11-09 09:55:10 +0000

    added delete attribute request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/DeleteAttributeRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class DeleteAttributeRequestValidator : AbstractValidator<DeleteAttributeRequest>
+{
+    public DeleteAttributeRequestValidator()
+    {
+        Include(new DatabaseCollectionIdAttributeKeyBaseRequestValidator<DeleteAttributeRequest, DeleteAttributeRequestValidator>());
+    }
+}
