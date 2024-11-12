265ff8ac9d7af11072bdcf412d4216dcbb05b48d
Matthew Parker <pingu@pinguapps.com>
2024-11-12 19:09:30 +0000

    Ensured that CreateDocumentRequest (non generic) cannot be instantiated without the builder

--- src/PinguApps.Appwrite.Shared/Requests/Databases/CreateDocumentRequest.cs
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/CreateDocumentRequest.cs
@@ -3,6 +3,8 @@
 namespace PinguApps.Appwrite.Shared.Requests.Databases;
 public class CreateDocumentRequest : CreateDocumentRequest<Dictionary<string, object?>>
 {
+    internal CreateDocumentRequest() { }
+
     /// <summary>
     /// Creates a new builder for creating a document request
     /// </summary>
