3ab0d08721cabde7305ab05a649ae209391666b4
Matthew Parker <pingu@pinguapps.com>
2024-11-12 19:13:35 +0000

    added intellisense docs for generic create document request

--- src/PinguApps.Appwrite.Shared/Requests/Databases/CreateDocumentRequestGeneric.cs
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/CreateDocumentRequestGeneric.cs
@@ -14,19 +14,19 @@ public class CreateDocumentRequest<TData> : DatabaseCollectionIdBaseRequest<Crea
     where TData : class
 {
     /// <summary>
-    /// 
+    /// Document ID. Choose a custom ID or generate a random ID with ID.unique(). Valid chars are a-z, A-Z, 0-9, period, hyphen, and underscore. Can't start with a special char. Max length is 36 chars
     /// </summary>
     [JsonPropertyName("documentId")]
     public string DocumentId { get; set; } = IdUtils.GenerateUniqueId();
 
     /// <summary>
-    /// 
+    /// Document data - Provide your own type to match your database schema, or build this up using <see cref="CreateDocumentRequest.CreateBuilder"/>
     /// </summary>
     [JsonPropertyName("data")]
     public TData Data { get; set; } = default!;
 
     /// <summary>
-    /// 
+    /// An array of permissions strings. By default, only the current user is granted all permissions. <see href="https://appwrite.io/docs/permissions">Learn more about permissions</see>.
     /// </summary>
     [JsonPropertyName("permissions")]
     [JsonConverter(typeof(PermissionListConverter))]
