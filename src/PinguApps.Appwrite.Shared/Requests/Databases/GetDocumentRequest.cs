943acc4c435ed14171bee97463cb0461915d5a14
Matthew Parker <pingu@pinguapps.com>
2024-11-12 19:16:02 +0000

    Added request for get document

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/GetDocumentRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using PinguApps.Appwrite.Shared.Attributes;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+using PinguApps.Appwrite.Shared.Utils;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases;
+public class GetDocumentRequest : DatabaseCollectionDocumentIdBaseRequest<GetDocumentRequest, GetDocumentRequestValidator>
+{
+    /// <summary>
+    /// Array of query strings generated using the Query class provided by the SDK. <see href="https://appwrite.io/docs/queries">Learn more about queries</see>. Maximum of 100 queries are allowed, each 4096 characters long.
+    /// </summary>
+    [JsonPropertyName("queries")]
+    [SdkExclude]
+    public List<Query>? Queries { get; set; } = null;
+}
