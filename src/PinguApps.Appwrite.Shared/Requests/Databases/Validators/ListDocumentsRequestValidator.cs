a72865476edc4cebfef49f5c20f12529fda73309
Matthew Parker <pingu@pinguapps.com>
2024-11-12 17:34:40 +0000

    Added request for list documents

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/ListDocumentsRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using PinguApps.Appwrite.Shared.Requests.Validators;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class ListDocumentsRequestValidator : AbstractValidator<ListDocumentsRequest>
+{
+    public ListDocumentsRequestValidator()
+    {
+        Include(new QueryBaseRequestValidator<ListDocumentsRequest, ListDocumentsRequestValidator>());
+
+        RuleFor(x => x.DatabaseId)
+            .NotEmpty()
+            .WithMessage("DatabaseId is required.");
+
+        RuleFor(x => x.CollectionId)
+            .NotEmpty()
+            .WithMessage("CollectionId is required.");
+    }
+}
