c9f422dcde43ff65dae17a92110e245ef53b5322
Matthew Parker <pingu@pinguapps.com>
2024-11-19 19:17:05 +0000

    added upper case enum converter

---
+++ src/PinguApps.Appwrite.Shared/Converters/UpperCaseEnumConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PinguApps.Appwrite.Shared.Converters;
+public class UpperCaseEnumConverter : JsonConverter<object>
+{
+    public override bool CanConvert(Type typeToConvert)
+    {
+        return typeToConvert.IsEnum;
+    }
+
+    public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        return Enum.Parse(typeToConvert, value, true);
+    }
+
+    public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString().ToUpper());
+    }
+}
