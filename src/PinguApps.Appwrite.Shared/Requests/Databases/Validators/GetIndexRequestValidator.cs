2a5932d68ba09482f20cf7fea702c471f0ccae32
Matthew Parker <pingu@pinguapps.com>
2024-11-21 19:03:58 +0000

    added get index request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/GetIndexRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class GetIndexRequestValidator : AbstractValidator<GetIndexRequest>
+{
+    public GetIndexRequestValidator()
+    {
+        Include(new DatabaseCollectionIdIndexKeyBaseRequestValidator<GetIndexRequest, GetIndexRequestValidator>());
+    }
+}
