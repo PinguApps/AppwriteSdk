f077778ffeb303beb2656ccf6f9404e5f8d8d9eb
Matthew Parker <pingu@pinguapps.com>
2024-11-21 19:24:40 +0000

    Opened the IDatabaseApi ready for adding more endpoints to

---
+++ src/PinguApps.Appwrite.Server/Internals/IDatabasesApi.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Responses;
+using Refit;
+
+namespace PinguApps.Appwrite.Server.Internals;
+internal interface IDatabasesApi : IBaseApi
+{
+    [Get("/databases")]
+    Task<IApiResponse<Database>> GetDatabase(GetDatabaseRequest request);
+}
