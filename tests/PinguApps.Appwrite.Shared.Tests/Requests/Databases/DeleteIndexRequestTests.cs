89ff014221f9b34cd92932418fcdd04f94f1f32c
Matthew Parker <pingu@pinguapps.com>
2024-11-21 19:09:12 +0000

    added delete index request tests

---
+++ tests/PinguApps.Appwrite.Shared.Tests/Requests/Databases/DeleteIndexRequestTests.cs
@@ -0,0 +1,8 @@
+﻿using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+
+namespace PinguApps.Appwrite.Shared.Tests.Requests.Databases;
+public class DeleteIndexRequestTests : DatabaseCollectionIdIndexKeyBaseRequestTests<DeleteIndexRequest, DeleteIndexRequestValidator>
+{
+    protected override DeleteIndexRequest CreateValidDatabaseCollectionIdIndexKeyRequest => new();
+}
