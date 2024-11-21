979a61b34bd61819c95d6910e135ff2d7f5848fe
Matthew Parker <pingu@pinguapps.com>
2024-11-21 19:07:39 +0000

    added delete index request

---
+++ src/PinguApps.Appwrite.Shared/Requests/Databases/Validators/DeleteIndexRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace PinguApps.Appwrite.Shared.Requests.Databases.Validators;
+public class DeleteIndexRequestValidator : AbstractValidator<DeleteIndexRequest>
+{
+    public DeleteIndexRequestValidator()
+    {
+        Include(new DatabaseCollectionIdIndexKeyBaseRequestValidator<DeleteIndexRequest, DeleteIndexRequestValidator>());
+    }
+}
