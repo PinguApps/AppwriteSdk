d0bf2cc3b024d4195bfbd1167538fbdff8fd53ad
Matthew Parker <pingu@pinguapps.com>
2024-11-23 20:54:13 +0000

    added databases client for client

---
+++ src/PinguApps.Appwrite.Client/Clients/DatabasesClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using PinguApps.Appwrite.Client.Internals;
+using PinguApps.Appwrite.Shared;
+using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Responses;
+
+namespace PinguApps.Appwrite.Client.Clients;
+
+/// <inheritdoc/>
+public class DatabasesClient : IDatabasesClient
+{
+    private readonly IDatabasesApi _databasesApi;
+
+    public DatabasesClient(IServiceProvider services)
+    {
+        _databasesApi = services.GetRequiredService<IDatabasesApi>();
+    }
+
+    [ExcludeFromCodeCoverage]
+    /// <inheritdoc/>
+    public Task<AppwriteResult<DocumentsList>> ListDocuments(ListDocumentsRequest request) => throw new NotImplementedException();
+
+    [ExcludeFromCodeCoverage]
+    /// <inheritdoc/>
+    public Task<AppwriteResult<Document>> CreateDocument(CreateDocumentRequest request) => throw new NotImplementedException();
+
+    [ExcludeFromCodeCoverage]
+    /// <inheritdoc/>
+    public Task<AppwriteResult> DeleteDocument(DeleteDocumentRequest request) => throw new NotImplementedException();
+
+    [ExcludeFromCodeCoverage]
+    /// <inheritdoc/>
+    public Task<AppwriteResult<Document>> GetDocument(GetDocumentRequest request) => throw new NotImplementedException();
+
+    [ExcludeFromCodeCoverage]
+    /// <inheritdoc/>
+    public Task<AppwriteResult<Document>> UpdateDocument(UpdateDocumentRequest request) => throw new NotImplementedException();
+}
