4c709881164611ea4664ace1c6fe47c118942521
Matthew Parker <pingu@pinguapps.com>
2024-11-23 20:47:49 +0000

    Added db client to appwrite client

--- src/PinguApps.Appwrite.Server/Clients/AppwriteClient.cs
+++ src/PinguApps.Appwrite.Server/Clients/AppwriteClient.cs
@@ -6,11 +6,13 @@ public class AppwriteClient : IAppwriteClient
     public IAccountClient Account { get; }
     public IUsersClient Users { get; }
     public ITeamsClient Teams { get; }
+    public IDatabasesClient DatabasesClient { get; }
 
-    public AppwriteClient(IAccountClient accountClient, IUsersClient usersClient, ITeamsClient teamsClient)
+    public AppwriteClient(IAccountClient accountClient, IUsersClient usersClient, ITeamsClient teamsClient, IDatabasesClient databasesClient)
     {
         Account = accountClient;
         Users = usersClient;
         Teams = teamsClient;
+        DatabasesClient = databasesClient;
     }
 }
--- src/PinguApps.Appwrite.Server/Clients/IAppwriteClient.cs
+++ src/PinguApps.Appwrite.Server/Clients/IAppwriteClient.cs
@@ -18,5 +18,20 @@ public interface IAppwriteClient
     /// <para><see href="https://appwrite.io/docs/references/1.6.x/server-rest/users">Appwrite Docs</see></para>
     /// </summary>
     IUsersClient Users { get; }
+
+    /// <summary>
+    /// <para>The Teams service allows you to group users of your project and to enable them to share <see href="https://appwrite.io/docs/advanced/platform/permissions">read and write</see> access to your project resources, such as database documents or storage files.</para>
+    /// <para>Each user who creates a team becomes the team owner and can delegate the ownership role by inviting a new team member. Only team owners can invite new users to their team.</para>
+    /// <para><see href="https://appwrite.io/docs/references/1.6.x/server-rest/teams">Appwrite Docs</see></para>
+    /// </summary>
     ITeamsClient Teams { get; }
+
+    /// <summary>
+    /// <para>The Databases service allows you to create structured collections of documents, query and filter lists of documents, and manage an advanced set of read and write access permissions.</para>
+    /// <para>All data returned by the Databases service are represented as structured JSON documents.</para>
+    /// <para>The Databases service can contain multiple databases, each database can contain multiple collections. A collection is a group of similarly structured documents. The accepted structure of documents is defined by <see href="https://appwrite.io/docs/products/databases/collections#attributes">collection attributes</see>. The collection attributes help you ensure all your user-submitted data is validated and stored according to the collection structure.</para>
+    /// <para>Using Appwrite permissions architecture, you can assign read or write access to each collection or document in your project for either a specific user, team, user role, or even grant it with public access (any). You can learn more about <see href="https://appwrite.io/docs/products/databases/permissions">how Appwrite handles permissions and access control</see>.</para>
+    /// <para><see href="https://appwrite.io/docs/references/1.6.x/server-rest/databases">Appwrite Docs</see></para>
+    /// </summary>
+    IDatabasesClient DatabasesClient { get; }
 }
