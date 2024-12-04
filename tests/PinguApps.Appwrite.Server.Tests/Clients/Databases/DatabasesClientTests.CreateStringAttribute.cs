72fb9a9f3b664b1e770c9fcc5f61198273f6762e
Matthew Parker <pingu@pinguapps.com>
2024-12-04 01:01:31 +0000

    added tests for create string attribute

--- tests/PinguApps.Appwrite.Server.Tests/Clients/Databases/DatabasesClientTests.CreateStringAttribute.cs
+++ tests/PinguApps.Appwrite.Server.Tests/Clients/Databases/DatabasesClientTests.CreateStringAttribute.cs
@@ -1,4 +1,81 @@
-﻿namespace PinguApps.Appwrite.Server.Tests.Clients.Databases;
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Tests;
+using PinguApps.Appwrite.Shared.Utils;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Server.Tests.Clients.Databases;
 public partial class DatabasesClientTests
 {
+    [Fact]
+    public async Task CreateStringAttribute_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        var request = new CreateStringAttributeRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "created",
+            Size = 100
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/attributes/string")
+            .ExpectedHeaders()
+            .Respond(TestConstants.AppJson, TestConstants.AttributeStringResponse);
+
+        // Act
+        var result = await _appwriteClient.Databases.CreateStringAttribute(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task CreateStringAttribute_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new CreateStringAttributeRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "created",
+            Size = 100
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/attributes/string")
+            .ExpectedHeaders()
+            .Respond(HttpStatusCode.BadRequest, TestConstants.AppJson, TestConstants.AppwriteError);
+
+        // Act
+        var result = await _appwriteClient.Databases.CreateStringAttribute(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task CreateStringAttribute_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new CreateStringAttributeRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "created",
+            Size = 100
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/attributes/string")
+            .ExpectedHeaders()
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteClient.Databases.CreateStringAttribute(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
 }
