d6417d8517e6cd7da145ab34b93edeb40b858fa8
Matthew Parker <pingu@pinguapps.com>
2024-12-04 00:56:39 +0000

    added tests for create ip attribute

--- tests/PinguApps.Appwrite.Server.Tests/Clients/Databases/DatabasesClientTests.CreateIpAttribute.cs
+++ tests/PinguApps.Appwrite.Server.Tests/Clients/Databases/DatabasesClientTests.CreateIpAttribute.cs
@@ -1,4 +1,78 @@
-﻿namespace PinguApps.Appwrite.Server.Tests.Clients.Databases;
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Tests;
+using PinguApps.Appwrite.Shared.Utils;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Server.Tests.Clients.Databases;
 public partial class DatabasesClientTests
 {
+    [Fact]
+    public async Task CreateIpAttribute_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        var request = new CreateIPAttributeRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "created"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/attributes/ip")
+            .ExpectedHeaders()
+            .Respond(TestConstants.AppJson, TestConstants.AttributeIpResponse);
+
+        // Act
+        var result = await _appwriteClient.Databases.CreateIpAttribute(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task CreateIpAttribute_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new CreateIPAttributeRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "created"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/attributes/ip")
+            .ExpectedHeaders()
+            .Respond(HttpStatusCode.BadRequest, TestConstants.AppJson, TestConstants.AppwriteError);
+
+        // Act
+        var result = await _appwriteClient.Databases.CreateIpAttribute(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task CreateIpAttribute_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new CreateIPAttributeRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "created"
+        };
+
+        _mockHttp.Expect(HttpMethod.Post, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/attributes/ip")
+            .ExpectedHeaders()
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteClient.Databases.CreateIpAttribute(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
 }
