3970d3f1fd6c1e93260b3399386d46ef79255f18
Matthew Parker <pingu@pinguapps.com>
2024-12-04 00:51:50 +0000

    added tests for update enum attribute

--- tests/PinguApps.Appwrite.Server.Tests/Clients/Databases/DatabasesClientTests.UpdateEnumAttribute.cs
+++ tests/PinguApps.Appwrite.Server.Tests/Clients/Databases/DatabasesClientTests.UpdateEnumAttribute.cs
@@ -1,4 +1,81 @@
-﻿namespace PinguApps.Appwrite.Server.Tests.Clients.Databases;
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Tests;
+using PinguApps.Appwrite.Shared.Utils;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Server.Tests.Clients.Databases;
 public partial class DatabasesClientTests
 {
+    [Fact]
+    public async Task UpdateEnumAttribute_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        var request = new UpdateEnumAttributeRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "attributeKey",
+            Elements = ["element1", "element2", "element3"]
+        };
+
+        _mockHttp.Expect(HttpMethod.Patch, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/attributes/enum/{request.Key}")
+            .ExpectedHeaders()
+            .Respond(TestConstants.AppJson, TestConstants.AttributeEnumResponse);
+
+        // Act
+        var result = await _appwriteClient.Databases.UpdateEnumAttribute(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task UpdateEnumAttribute_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new UpdateEnumAttributeRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "attributeKey",
+            Elements = ["element1", "element2", "element3"]
+        };
+
+        _mockHttp.Expect(HttpMethod.Patch, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/attributes/enum/{request.Key}")
+            .ExpectedHeaders()
+            .Respond(HttpStatusCode.BadRequest, TestConstants.AppJson, TestConstants.AppwriteError);
+
+        // Act
+        var result = await _appwriteClient.Databases.UpdateEnumAttribute(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task UpdateEnumAttribute_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new UpdateEnumAttributeRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Key = "attributeKey",
+            Elements = ["element1", "element2", "element3"]
+        };
+
+        _mockHttp.Expect(HttpMethod.Patch, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/attributes/enum/{request.Key}")
+            .ExpectedHeaders()
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteClient.Databases.UpdateEnumAttribute(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
 }
