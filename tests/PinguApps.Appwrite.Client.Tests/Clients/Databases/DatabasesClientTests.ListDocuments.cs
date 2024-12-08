3321755fd4f08d9e5936713dfdfece3f63723716
Matthew Parker <pingu@pinguapps.com>
2024-12-08 00:38:19 +0000

    added tests for client List Documents

---
+++ tests/PinguApps.Appwrite.Client.Tests/Clients/Databases/DatabasesClientTests.ListDocuments.cs
@@ -0,0 +1,99 @@
+﻿using System.Net;
+using PinguApps.Appwrite.Shared.Requests.Databases;
+using PinguApps.Appwrite.Shared.Tests;
+using PinguApps.Appwrite.Shared.Utils;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Client.Tests.Clients.Databases;
+public partial class DatabasesClientTests
+{
+    [Fact]
+    public async Task ListDocuments_ShouldReturnSuccess_WhenApiCallSucceeds()
+    {
+        // Arrange
+        var request = new ListDocumentsRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId()
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/documents")
+            .ExpectedHeaders()
+            .Respond(TestConstants.AppJson, TestConstants.DocumentsListResponse);
+
+        // Act
+        var result = await _appwriteClient.Databases.ListDocuments(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task ListDocuments_ShouldProvideQueryParams_WhenQueriesProvided()
+    {
+        // Arrange
+        var query = Query.Limit(5);
+        var request = new ListDocumentsRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId(),
+            Queries = [query]
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/documents")
+            .ExpectedHeaders()
+            .WithQueryString($"queries[]={query.GetQueryString()}")
+            .Respond(TestConstants.AppJson, TestConstants.DocumentsListResponse);
+
+        // Act
+        var result = await _appwriteClient.Databases.ListDocuments(request);
+
+        // Assert
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async Task ListDocuments_ShouldHandleException_WhenApiCallFails()
+    {
+        // Arrange
+        var request = new ListDocumentsRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId()
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/documents")
+            .ExpectedHeaders()
+            .Respond(HttpStatusCode.BadRequest, TestConstants.AppJson, TestConstants.AppwriteError);
+
+        // Act
+        var result = await _appwriteClient.Databases.ListDocuments(request);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.True(result.IsAppwriteError);
+    }
+
+    [Fact]
+    public async Task ListDocuments_ShouldReturnErrorResponse_WhenExceptionOccurs()
+    {
+        // Arrange
+        var request = new ListDocumentsRequest
+        {
+            DatabaseId = IdUtils.GenerateUniqueId(),
+            CollectionId = IdUtils.GenerateUniqueId()
+        };
+
+        _mockHttp.Expect(HttpMethod.Get, $"{TestConstants.Endpoint}/databases/{request.DatabaseId}/collections/{request.CollectionId}/documents")
+            .ExpectedHeaders()
+            .Throw(new HttpRequestException("An error occurred"));
+
+        // Act
+        var result = await _appwriteClient.Databases.ListDocuments(request);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.True(result.IsInternalError);
+        Assert.Equal("An error occurred", result.Result.AsT2.Message);
+    }
+}
---
+++ tests/PinguApps.Appwrite.Client.Tests/Clients/Databases/DatabasesClientTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using PinguApps.Appwrite.Shared.Converters;
+using PinguApps.Appwrite.Shared.Tests;
+using Refit;
+using RichardSzalay.MockHttp;
+
+namespace PinguApps.Appwrite.Client.Tests.Clients.Databases;
+public partial class DatabasesClientTests
+{
+    private readonly MockHttpMessageHandler _mockHttp;
+    private readonly IAppwriteClient _appwriteClient;
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+    public DatabasesClientTests()
+    {
+        _mockHttp = new MockHttpMessageHandler();
+        var services = new ServiceCollection();
+
+        services.AddAppwriteClientForServer(TestConstants.ProjectId, TestConstants.Endpoint, new RefitSettings
+        {
+            HttpMessageHandlerFactory = () => _mockHttp
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        _appwriteClient = serviceProvider.GetRequiredService<IAppwriteClient>();
+
+        _jsonSerializerOptions = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        _jsonSerializerOptions.Converters.Add(new IgnoreSdkExcludedPropertiesConverterFactory());
+    }
+}
