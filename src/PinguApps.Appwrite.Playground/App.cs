0c869087b58ae8e26572cee4401d8451f6450cf2
Matthew Parker <pingu@pinguapps.com>
2024-12-10 19:15:16 +0000

    Testing the before / after builder pattern for update document

--- src/PinguApps.Appwrite.Playground/App.cs
+++ src/PinguApps.Appwrite.Playground/App.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
 using PinguApps.Appwrite.Shared.Requests.Databases;
 
 namespace PinguApps.Appwrite.Playground;
@@ -15,13 +16,25 @@ internal class App
         _session = config.GetValue<string>("Session");
     }
 
+    private class Rec
+    {
+        [JsonPropertyName("test")]
+        public string Test { get; set; } = string.Empty;
+
+        [JsonPropertyName("boolAttribute")]
+        public bool BoolAttribute { get; set; }
+    }
+
     public async Task Run(string[] args)
     {
+        var before = new Rec { Test = "test", BoolAttribute = false };
+        var after = new Rec { Test = "test", BoolAttribute = true };
+
         var request = UpdateDocumentRequest.CreateBuilder()
             .WithDatabaseId("67541a2800221703e717")
             .WithCollectionId("67541a37001514b81821")
             .WithDocumentId("67541af9000055e59e59")
-            .AddField("test", "Server Update")
+            .WithChanges(before, after)
             .Build();
 
         var serverResponse = await _server.Databases.UpdateDocument(request);
@@ -33,14 +46,14 @@ internal class App
 
         Console.WriteLine("###############################################################################");
 
-        Console.ReadKey();
-        request.Data["test"] = "Client Update";
+        //Console.ReadKey();
+        //request.Data["test"] = "Client Update";
 
-        var clientResponse = await _client.Databases.UpdateDocument(request);
+        //var clientResponse = await _client.Databases.UpdateDocument(request);
 
-        Console.WriteLine(clientResponse.Result.Match(
-            result => result.ToString(),
-            appwriteError => appwriteError.Message,
-            internalError => internalError.Message));
+        //Console.WriteLine(clientResponse.Result.Match(
+        //    result => result.ToString(),
+        //    appwriteError => appwriteError.Message,
+        //    internalError => internalError.Message));
     }
 }
