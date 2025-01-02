325c36f4b50aaf962dc0dcd1245f042968e074c6
Matthew Parker <pingu@pinguapps.com>
2025-01-02 01:39:49 +0000

    removed unused method

--- src/PinguApps.Appwrite.Shared/Converters/DocumentGenericConverter.cs
+++ src/PinguApps.Appwrite.Shared/Converters/DocumentGenericConverter.cs
@@ -173,21 +173,6 @@ public class DocumentGenericConverter<TData> : JsonConverter<Document<TData>>
         return dict;
     }
 
-    private static void ValidateRequiredFields(DocumentFields fields)
-    {
-        if (fields.Id is null)
-            throw new JsonException("Unable to find a value for Id");
-
-        if (fields.CollectionId is null)
-            throw new JsonException("Unable to find a value for CollectionId");
-
-        if (fields.DatabaseId is null)
-            throw new JsonException("Unable to find a value for DatabaseId");
-
-        if (fields.Permissions is null)
-            throw new JsonException("Unable to find a value for Permissions");
-    }
-
     private static TData DeserializeCustomData(Dictionary<string, object?> dataProperties, JsonSerializerOptions options)
     {
         var dataJson = JsonSerializer.Serialize(dataProperties, options);
