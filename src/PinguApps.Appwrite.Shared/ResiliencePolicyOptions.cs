9ad6c017898a5697f7db46bceb56da2afc950e65
codefactor-io <support@codefactor.io>
2025-01-16 18:19:57 +0000

    [CodeFactor] Apply fixes

--- src/PinguApps.Appwrite.Shared/ResiliencePolicyOptions.cs
+++ src/PinguApps.Appwrite.Shared/ResiliencePolicyOptions.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 
 namespace PinguApps.Appwrite.Shared;
 public class ResiliencePolicyOptions
 {
     public int RetryCount { get; set; } = 3;
-    public Func<int, TimeSpan> SleepDurationProvider { get; set; } = retryAttempt => TimeSpan.FromSeconds((retryAttempt - 1) * 2 + 1);
+    public Func<int, TimeSpan> SleepDurationProvider { get; set; } = retryAttempt => TimeSpan.FromSeconds(((retryAttempt - 1) * 2) + 1);
     public int CircuitBreakerThreshold { get; set; } = 5;
     public int CircuitBreakerDurationSeconds { get; set; } = 30;
     public bool DisableResilience { get; set; } = false;
