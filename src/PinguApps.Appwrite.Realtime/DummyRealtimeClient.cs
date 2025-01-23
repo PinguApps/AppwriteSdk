d9a3e438b31d481db087000b49b114a915d638aa
Matthew Parker <pingu@pinguapps.com>
2025-01-23 22:11:16 +0000

    added dummy client for server usage

---
+++ src/PinguApps.Appwrite.Realtime/DummyRealtimeClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PinguApps.Appwrite.Realtime.Models;
+
+namespace PinguApps.Appwrite.Realtime;
+internal class DummyRealtimeClient : IRealtimeClient
+{
+    public bool IsConnected => false;
+
+    public Task DisconnectAsync() => Task.CompletedTask;
+
+    public void SetSession(string? session)
+    {
+    }
+
+    public IDisposable Subscribe<T>(List<string> channels, Action<RealtimeResponseEvent<T>> callback) => new DummyDisposable();
+
+    public IDisposable Subscribe<T>(string channel, Action<RealtimeResponseEvent<T>> callback) => new DummyDisposable();
+
+    private class DummyDisposable : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
--- src/PinguApps.Appwrite.Realtime/ServiceCollectionExtensions.cs
+++ src/PinguApps.Appwrite.Realtime/ServiceCollectionExtensions.cs
@@ -8,6 +8,14 @@ using PinguApps.Appwrite.Shared;
 namespace PinguApps.Appwrite.Realtime;
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds Appwrite Realtime SDK client to services so that you can inject IRealtimeClient
+    /// </summary>
+    /// <param name="services">The service collection to add to</param>
+    /// <param name="projectId">Your Appwrite Project ID</param>
+    /// <param name="endpoint">Your Appwrite Endpoint. Defaults to the cloud endpoint.</param>
+    /// <param name="configureOptions">Configure the options here</param>
+    /// <returns>The service collection, enabling chaining</returns>
     public static IServiceCollection AddAppwriteRealtime(this IServiceCollection services, string projectId, string endpoint = "wss://cloud.appwrite.io/v1", Action<WebSocketOptions>? configureOptions = null)
     {
         var realtimeEndpoint = GetWebsocketUrl(endpoint);
@@ -32,7 +40,18 @@ public static class ServiceCollectionExtensions
         return services;
     }
 
-    public static string GetWebsocketUrl(string url)
+    /// <summary>
+    /// Adds a dummy Realtime client to services so that you can inject IRealtimeClient, but where no effect will occur
+    /// </summary>
+    /// <param name="services">The service collection to add to</param>
+    /// <returns>The service collection, enabling chaining</returns>
+    public static IServiceCollection AddAppwriteRealtimeDummy(this IServiceCollection services)
+    {
+        services.AddSingleton<IRealtimeClient, DummyRealtimeClient>();
+        return services;
+    }
+
+    private static string GetWebsocketUrl(string url)
     {
         var result = url
             .Replace("https://", "wss://")
