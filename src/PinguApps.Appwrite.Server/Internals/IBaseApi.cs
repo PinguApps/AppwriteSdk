a0337bd641566b1e39cc999f8a3ebbcd0b1cc61b
Matthew Parker <pingu@pinguapps.com>
2025-03-02 11:56:43 +0000

    Changed casing for all headers - hopefully appeases firefox options request

--- src/PinguApps.Appwrite.Server/Internals/IBaseApi.cs
+++ src/PinguApps.Appwrite.Server/Internals/IBaseApi.cs
@@ -3,11 +3,11 @@ using Refit;
 
 namespace PinguApps.Appwrite.Server.Internals;
 
-[Headers("content-type: application/json",
-    "x-sdk-name: .NET",
-    "x-sdk-platform: server",
-    "x-sdk-language: dotnet",
-    $"x-sdk-version: {Constants.Version}",
+[Headers("Content-Type: application/json",
+    "X-Sdk-Name: .NET",
+    "X-Sdk-Platform: server",
+    "X-Sdk-Language: dotnet",
+    $"X-Sdk-Version: {Constants.Version}",
     "X-Appwrite-Response-Format: 1.6.0")]
 internal interface IBaseApi
 {
